584e7e4f9f56e888e8a529c3eedb608b3098de91
Orphen <sergiramirezgarcia@gmail.com>
2018-02-17 20:04:25 +0100

    First commit, add .gitignore

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	#region Public Data
+	
+	#endregion
+	
+	#region Private Serialized Fields
+	
+	#endregion
+	
+	#region Private Non-Serialized Fields
+	
+	#endregion
+	
+	#region Properties
+
+    #endregion
+	
+	#region MonoBehaviour Methods
+	
+	#endregion
+	
+	#region Public Methods
+	
+	#endregion
+	
+	#region Private Methods
+	
+	#endregion
+}
