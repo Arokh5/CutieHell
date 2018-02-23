f581d25e5b7c59d15e0640c1a4956226922a9fa7
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-02-23 23:20:00 +0100

    Improve the conquering effect and use testing models with textures

--- Assets/Scripts/conquerable-buildings/ConquerableBuilding.cs
+++ Assets/Scripts/conquerable-buildings/ConquerableBuilding.cs
@@ -4,124 +4,231 @@ using UnityEngine;
 
 public class ConquerableBuilding : MonoBehaviour {
 
+    [Header("Elements setup")]
 
-    private MeshRenderer meshRenderer;
     [SerializeField]
-    private MeshRenderer areaOfEffect;
+    private MeshRenderer buildingRenderer;
+    [SerializeField]
+    private MeshRenderer alternateBuildingRenderer;
+    [SerializeField]
+    private MeshRenderer areaRenderer;
 
     [Header("Life and stuff")]
-    public int initialHitPoints = 50;
-    [SerializeField]
-	private int hitPoints;
+    [Tooltip("The initial amount of hit points for the conquerable building.")]
+    public float initialHitPoints = 50;
+    // Duration in seconds
+    [Tooltip("The duration (in seconds) for which the conquerable object is considered to be \"under attack\" after the last actual attack happened.")]
+    public float underAttackStateDuration = 1;
+    [Tooltip("The duration (in seconds) that the dark to cute conversion takes.")]
+    public float conquerEffectDuration = 1;
+
+    private float underAttackElapsedTime = 0;
+    [SerializeField]    // TEST
+    private float conquerEffectElapsedTime = 0;
+    [SerializeField]    // TEST
+	private float hitPoints;
+    private bool underAttack = false;
+    private bool conquering = false;
     private bool conquered = false;
 
-    [Header("Damage testing")]
-    public bool reset = false;
-    public bool loseHitPoints = false;
-    public int lifeLossPerSecond = 0;
-    private float lifeToLose = 0;
+    [Header("Model shaking")]
+    [Range(0, 1)]
+    public float shakeAmplitude;
+    public float shakeSpeed;
 
-    [Header("Life Restore testing")]
+    [Header("Damage testing")]
+    public bool reset = false; // TEST
+    public bool loseHitPoints = false; // TEST
+    public int lifeLossPerSecond = 0; // TEST
     public bool restoreLife = false;
-    public int lifeToRestore = 0;
+    
 
     private void OnApplicationQuit()
     {
-        ResetAutomaticLifeLoss();
+        Reset();
     }
 
-    void Awake ()
+    private void Awake()
     {
-        meshRenderer = GetComponent<MeshRenderer>();
+        UnityEngine.Assertions.Assert.IsNotNull(buildingRenderer, "Building Renderer not assigned for ConquerableBuilding script in GameObject " + gameObject.name);
+        UnityEngine.Assertions.Assert.IsNotNull(alternateBuildingRenderer, "Alternate Building Renderer not assigned for ConquerableBuilding script in GameObject " + gameObject.name);
+        UnityEngine.Assertions.Assert.IsNotNull(areaRenderer, "Area Renderer not assigned for ConquerableBuilding script in GameObject " + gameObject.name);
+        buildingRenderer.gameObject.SetActive(true);
+        alternateBuildingRenderer.gameObject.SetActive(false);
+        areaRenderer.gameObject.SetActive(true);
     }
 
-	void Start ()
+    void Start ()
     {
+        Reset();
         hitPoints = initialHitPoints;
-	}
+        buildingRenderer.material.SetFloat("_ShakeAmplitude", shakeAmplitude);
+        buildingRenderer.material.SetFloat("_ShakeSpeed", shakeSpeed);
+    }
 	
 	// Update is called once per frame
 	void Update ()
+    {
+        Test();
+
+        if (underAttack)
+        {
+            underAttackElapsedTime += Time.deltaTime;
+            if (underAttackElapsedTime >= underAttackStateDuration)
+            {
+                SetUnderAttack(false);
+                underAttackElapsedTime = 0;
+            }
+        }
+
+        if (conquering)
+        {
+            conquerEffectElapsedTime += Time.deltaTime;
+            if (conquerEffectElapsedTime < conquerEffectDuration)
+            {
+                ConquerEffect();
+            }
+            else
+            {
+                Conquer();
+            }
+        }
+	}
+
+    void Test()
     {
         if (reset)
         {
             reset = false;
             loseHitPoints = false;
-            ResetAutomaticLifeLoss();
+            Reset();
         }
 
         if (restoreLife)
         {
             restoreLife = false;
-            RestoreHitPoints(lifeToRestore);
+            RestoreHitPoints();
         }
 
         if (loseHitPoints)
         {
-            lifeToLose += lifeLossPerSecond * Time.deltaTime;
-            if (lifeToLose > 1)
-            {
-                lifeToLose -= 1;
-                TakeDamage(1);
-                if (conquered)
-                    loseHitPoints = false;
-            }
+            TakeDamage(lifeLossPerSecond * Time.deltaTime);
+            if (conquered)
+                loseHitPoints = false;
         }
-        
-	}
+    }
 
-    void TakeDamage(int damage)
+    void TakeDamage(float damage)
     {
         if (conquered)
             return;
 
+        // Reset the currentUnderAttackElapsedTime timer
+        SetUnderAttack(true);
+        underAttackElapsedTime = 0;
+
         hitPoints -= damage;
+        
+
         if (hitPoints <= 0)
         {
             hitPoints = 0;
-            Conquer();
         }
 
-        AdjustMaterial();
+        AdjustMaterials();
+
+        if (hitPoints == 0)
+        {
+            ConquerEffect();
+        }
+
     }
 
-    void RestoreHitPoints (int toRestore)
+    void SetUnderAttack(bool underAttackState)
     {
-        hitPoints += toRestore;
-        if (hitPoints > initialHitPoints)
-            hitPoints = initialHitPoints;
+        if (underAttack != underAttackState)
+        {
+            if (underAttackState)
+            {
+                buildingRenderer.material.SetFloat("_UnderAttack", 1);
+            }
+            else
+            {
+                buildingRenderer.material.SetFloat("_UnderAttack", 0);
+            }
+        }
+        underAttack = underAttackState;
+    }
 
-        AdjustMaterial();
+    void RestoreHitPoints ()
+    {
+        hitPoints = initialHitPoints;
 
-        if (conquered && hitPoints > 0)
-            Unconquer();
+        AdjustMaterials();
+
+        if (conquered)
+        {
+            Reset();
+        }
+    }
+
+    void ConquerEffect()
+    {
+        if (!conquering)
+        {
+            conquering = true;
+            conquerEffectElapsedTime = 0;
+            buildingRenderer.material.SetFloat("_ShakeAmplitude", 0);
+            alternateBuildingRenderer.material.SetFloat("_SizeFactor", 0);
+            alternateBuildingRenderer.gameObject.SetActive(true);
+        }
+
+        float progress = conquerEffectElapsedTime / conquerEffectDuration;
+        areaRenderer.material.SetFloat("_AlternateStartRadius", progress);
+
+        if (progress < 0.5f)
+        {
+            buildingRenderer.material.SetFloat("_ShakeAmplitude", progress * 2);
+        }
+        else if (progress < 0.6f)
+        {
+            alternateBuildingRenderer.material.SetFloat("_SizeFactor", (progress - 0.5f) * 4);
+        }
+        else
+        {
+            // Rescale te progress to fall in the range [0,1]
+            progress = (progress - 0.6f) / (1 - 0.6f);
+            alternateBuildingRenderer.material.SetFloat("_SizeFactor", 1 + (0.2f / (1 + progress)) * Mathf.Sin(2 * Mathf.PI * 2 * progress));
+        }
     }
 
     void Conquer()
     {
+        conquering = false;
+        conquerEffectElapsedTime = 0;
+        alternateBuildingRenderer.material.SetFloat("_SizeFactor", 1);
+        areaRenderer.material.SetFloat("_Conquered", 1);
         conquered = true;
-        areaOfEffect.material.SetFloat("_Blend", 1);
     }
 
-    void Unconquer()
+    void AdjustMaterials()
     {
-        conquered = false;
-        areaOfEffect.material.SetFloat("_Blend", 0);
+        float conqueredFactor = (initialHitPoints - hitPoints) / (float)initialHitPoints;
+        buildingRenderer.material.SetFloat("_ConquerFactor", conqueredFactor);
+        areaRenderer.material.SetFloat("_ConquerFactor", conqueredFactor);
     }
 
-    void AdjustMaterial()
-    {
-        float blendFactor = (initialHitPoints - hitPoints) / (float)initialHitPoints;
-        meshRenderer.material.SetFloat("_Blend", blendFactor);
-    }
-
-    void ResetAutomaticLifeLoss()
+    void Reset()
     {
         hitPoints = initialHitPoints;
-        lifeToLose = 0;
-        meshRenderer.material.SetFloat("_Blend", 0);
+        buildingRenderer.material.SetFloat("_ConquerFactor", 0);
+        buildingRenderer.material.SetFloat("_ShakeAmplitude", shakeAmplitude);
+        buildingRenderer.material.SetFloat("_ShakeSpeed", shakeSpeed);
+        alternateBuildingRenderer.material.SetFloat("_SizeFactor", 0);
+        areaRenderer.material.SetFloat("_AlternateStartRadius", 0);
+        areaRenderer.material.SetFloat("_ConquerFactor", 0);
+        areaRenderer.material.SetFloat("_Conquered", 0);
 
         conquered = false;
-        areaOfEffect.material.SetFloat("_Blend", 0);
     }
 }
---
+++ Assets/Shaders/AreaOfEffect.shader
@@ -0,0 +1,93 @@
+﻿Shader "Custom/Area Of Effect" {
+	Properties{
+		_Color("Color", Color) = (1,1,1,1)
+		_AlternateStartRadius("Alternate Start", Range(0,1)) = 0.0
+		_ConquerFactor("Conquer factor", Range(0,1)) = 0.0
+		[Toggle(CONQUERED)]
+		_Conquered("Conquered", Float) = 0
+		_InitialTex("Initial Texture", 2D) = "white" {}
+		_AlternateTex("Alternate Texture", 2D) = "white" {}
+		_Glossiness("Smoothness", Range(0,1)) = 0.5
+		_Metallic("Metallic", Range(0,1)) = 0.0
+	}
+		SubShader{
+		Tags{ "Queue" = "Transparent" "RenderType" = "Transparent" }
+		LOD 200
+
+		ZWrite Off
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		CGPROGRAM
+		// Physically based Standard lighting model, and enable shadows on all light types
+		#pragma surface surf Standard fullforwardshadows alpha
+
+		// Use shader model 3.0 target, to get nicer looking lighting
+		#pragma target 3.0
+
+		sampler2D _InitialTex;
+		sampler2D _AlternateTex;
+
+		struct Input {
+			float2 uv_InitialTex;
+			float2 uv_AlternateTex;
+		};
+
+		half _AlternateStartRadius;
+		half _ConquerFactor;
+		half _Conquered;
+		half _Glossiness;
+		half _Metallic;
+		fixed4 _Color;
+
+		// Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
+		// See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
+		// #pragma instancing_options assumeuniformscaling
+		UNITY_INSTANCING_BUFFER_START(Props)
+		// put more per-instance properties here
+		UNITY_INSTANCING_BUFFER_END(Props)
+
+		void surf(Input IN, inout SurfaceOutputStandard o) {
+			// Albedo comes from a texture tinted by color
+			fixed4 c;
+			if (_Conquered == 0)
+			{
+				float2 centeredUVs = IN.uv_InitialTex - float2(0.5, 0.5);
+				float radiusModifier = sin(atan2(centeredUVs.y, centeredUVs.x) * 8 + 0.5 *  _Time.w) * 0.04;
+				float startRadius = _AlternateStartRadius / 2;
+				startRadius += radiusModifier * startRadius;
+				float radius = _ConquerFactor / 2;
+				radius += radiusModifier * radius;
+				float texRadius = sqrt(centeredUVs.x * centeredUVs.x + centeredUVs.y * centeredUVs.y);
+				if (texRadius < startRadius)
+				{
+					c = tex2D(_AlternateTex, IN.uv_AlternateTex);
+				}
+				else if (texRadius < radius)
+				{
+					float progress = (texRadius - startRadius) / (radius - startRadius);
+					c = lerp(tex2D(_AlternateTex, IN.uv_AlternateTex), tex2D(_InitialTex, IN.uv_InitialTex), progress);
+				}
+				else
+				{
+					c = tex2D(_InitialTex, IN.uv_InitialTex);
+					//c = lerp(tex2D(_MainTex, IN.uv_MainTex), tex2D(_MainTex2, IN.uv_MainTex2), _ConquerFactor) * _Color;
+				}
+			}
+			else
+			{
+				c = tex2D(_AlternateTex, IN.uv_AlternateTex);
+			}
+			
+			if (c.a == 0) discard;
+
+			o.Albedo = c.rgb;
+			// Metallic and smoothness come from slider variables
+			o.Metallic = _Metallic;
+			o.Smoothness = _Glossiness;
+			o.Alpha = c.a;
+
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
