64d9234ab1556e6f9f86c78360e5d3f1b8865201
Orphen <sergiramirezgarcia@gmail.com>
2018-02-28 20:02:56 +0100

    Add basic attack cooldown

--- Assets/Scripts/combat-system/FocusAttack.cs
+++ Assets/Scripts/combat-system/FocusAttack.cs
@@ -9,6 +9,8 @@ public class FocusAttack : MonoBehaviour
     [SerializeField]
     private LayerMask layerMask;
     private RaycastHit hit;
+    private const float basicAttackCooldown = 0.5f;
+    private float time = basicAttackCooldown;
 
     #endregion
 
@@ -20,18 +22,25 @@ public class FocusAttack : MonoBehaviour
 
     private void Update()
     {
+        time += Time.deltaTime;
+
         if (InputManager.instance.GetXButtonDown())
         {
-            Debug.DrawRay(transform.position, transform.forward * 100, Color.red, 2);
+            if (time >= basicAttackCooldown)
+            {
+                Debug.DrawRay(transform.position, transform.forward * 100, Color.red, 2);
 
-            if (Physics.Raycast(transform.position, transform.forward, out hit, 100, layerMask.value))
-            {
-                GetComponent<InstantiateAttack>().InstantiateRedOrb(hit.transform);
-                Debug.Log("Hit enemy");
-            }
-            else
-            {
-                GetComponent<InstantiateAttack>().InstantiateRedOrb(null);
+                if (Physics.Raycast(transform.position, transform.forward, out hit, 100, layerMask.value))
+                {
+                    GetComponent<InstantiateAttack>().InstantiateRedOrb(hit.transform);
+                    Debug.Log("Hit enemy");
+                }
+                else
+                {
+                    GetComponent<InstantiateAttack>().InstantiateRedOrb(null);
+                }
+
+                time = 0f;
             }
         }
     }
