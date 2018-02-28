d1511f93a9df5e30e290826131004e37c0e15ad5
Mac092 <m.rodmur@gmail.com>
2018-02-28 09:20:32 +0100

    Add enemies spawn logic to the battlefield area Create enemies prefabs first version Create logic for the enemies pursue behaviour (first iteration) Fix ConquerableElement absence on the prior commit

---
+++ Assets/Scripts/IA/GameFlow/EnemyBehaviour.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using System;
+
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    private string m_AISubTeamID;
+
+    private Transform target;
+    private String area;
+
+    NavMeshAgent agent;
+    List<GameObject> spawnedElements = new List<GameObject>();
+
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        //this.transform.parent = GameObject.Find("Area1").transform;
+    }
+
+    public void InitializeTarget()
+    {
+        //TODO primero mirar si la structura conquistable no ha sido conquistada, si no hay ninguna torreta activa
+        GameObject areaParent = transform.parent.transform.parent.gameObject;
+        target = areaParent.GetComponent<GameAreaManager>().defensePoint.transform; 
+    }
+
+
+    public void UpdateTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target != null) 
+        {
+            Vector3 destination = new Vector3(target.position.x, this.transform.position.y, target.position.z); //The Y will be the own to avoid problems with mesh and not valid y values
+            agent.SetDestination(destination);
+        }
+        
+    }
+
+    public void Clustering()
+    {
+        GameObject element;
+        for (int i = 0; i < spawnedElements.Count; i++)
+        {
+            element = spawnedElements[i];
+            if (this.m_AISubTeamID.Equals(element.GetComponentInChildren<EnemyBehaviour>().getAISubTeamID()))
+            {
+                if (this.transform != element.transform && Vector3.Distance(this.transform.position, element.transform.position) > 4 && Vector3.Distance(this.transform.position, element.transform.position) < 7)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, element.transform.position, 0.01f);
+                }
+            }
+        }
+    }
+
+    public void SetArea(String paramArea)
+    {
+        area = paramArea;
+    }
+    public String GetArea()
+    {
+        return area;
+    }
+
+    public string getAISubTeamID()
+    {
+        return m_AISubTeamID;
+    }
+
+    public void setAISubTeamID(string subTeamID)
+    {
+        m_AISubTeamID = subTeamID;
+    }
+}
---
+++ Assets/Scripts/IA/GameFlow/GameScenariosManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScenariosManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            transform.Find("Area1").Find("ConquerableStructure1").GetComponent<ConquerableElement>().SetConquered(!transform.Find("Area1").Find("ConquerableStructure1").GetComponent<ConquerableElement>().GetConquered()); //TODO substitute the hardcoded
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (transform.Find("Area1").Find("weakTrapArea1_1").GetComponent<ConquerableElement>().GetActive() == true)
+            {
+                transform.Find("Area1").Find("weakTrapArea1_1").GetComponent<ConquerableElement>().SetBeingUsed(true);
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            if (transform.Find("Area1").Find("weakTrapArea1_1").GetComponent<ConquerableElement>().GetActive() == true)
+            {
+                transform.Find("Area1").Find("weakTrapArea1_1").GetComponent<ConquerableElement>().SetBeingUsed(false);
+            }
+        }
+    }
+}
