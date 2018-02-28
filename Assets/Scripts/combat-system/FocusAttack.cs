81cb5398ec347d5ff03806942b236fb70c7aa902
Orphen <sergiramirezgarcia@gmail.com>
2018-02-28 16:05:34 +0100

    Use raycast to check if Vlad hit enemies

---
+++ Assets/Scripts/combat-system/FocusAttack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FocusAttack : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private LayerMask layerMask;
+
+    #endregion
+
+    #region Properties
+
+    #endregion
+
+    #region MonoBehaviour Methods
+
+    private void Update()
+    {
+        if (InputManager.instance.GetXButtonDown())
+        {
+            Debug.DrawRay(transform.position, player.transform.forward * 100, Color.red, 2);
+
+            if (Physics.Raycast(transform.position, player.transform.forward, 100, layerMask.value))
+            {
+                Debug.Log("Hit enemy");
+            }
+        }
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    #endregion
+
+    #region Private Methods
+
+    #endregion
+}
