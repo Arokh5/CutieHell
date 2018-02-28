817b7cfb87a052ecf8fba5b5a5605bb9c98fb50e
Dani Reyes <dani-kingdom@hotmail.com>
2018-02-28 20:07:06 +0100

    Add simple camera collision with enviroment

--- Assets/Scripts/character-controller/CameraController.cs
+++ Assets/Scripts/character-controller/CameraController.cs
@@ -14,12 +14,14 @@ public class CameraController : MonoBehaviour {
     private const float yMaxLimit = 40f;
     private const float lerpSpeed = 0.1f;
 
+    private int collisionLayers;
     private float distance;
     private float x;
     private float y;
 
     private void Awake()
     {
+        collisionLayers = 1 << 4;
         distance = 3.0f;
         x = 0f;
         y = 0f;
@@ -47,7 +49,23 @@ public class CameraController : MonoBehaviour {
         y = ClampAngle(y, yMinLimit, yMaxLimit);
 
         Quaternion rotation = Quaternion.Euler(y, x, 0);
-        Vector3 position = rotation * new Vector3(0.5f, 1.75f, -distance) + player.position;
+
+        /* Checks for collisions */
+        float noCollisionDistance = distance;
+        Vector3 baseTempPosition = rotation * new Vector3(0.5f, 1.75f, -noCollisionDistance) + player.position;
+
+        for( float zOffset = distance; zOffset >= 0.5f; zOffset -= 0.1f) 
+        {
+            noCollisionDistance = zOffset;
+
+            if (DoubleViewingPosCheck(baseTempPosition + rotation * new Vector3(0.5f, 1.75f, -noCollisionDistance), zOffset)) {
+                break;
+            }
+        }
+
+        /* Ends collision detection */
+
+        Vector3 position = rotation * new Vector3(0.5f, 1.75f, -noCollisionDistance) + player.position;
 
         transform.position = position;
 
@@ -83,5 +101,39 @@ public class CameraController : MonoBehaviour {
             angle -= 360;
 
         return Mathf.Clamp(angle, min, max);
+    }
+
+    private bool DoubleViewingPosCheck(Vector3 checkPos, float offset) 
+    {
+        float playerFocusHeight = player.GetComponent<CapsuleCollider>().height * 0.5f;
+        return ViewingPosCheck(checkPos, playerFocusHeight);
+    }
+
+    // Check for collision from camera to player.
+    private bool ViewingPosCheck(Vector3 checkPos, float deltaPlayerHeight) {
+        RaycastHit hit;
+        // If a raycast from the check position to the player hits something...
+        if (Physics.Raycast(checkPos, player.position + (Vector3.up * deltaPlayerHeight) - checkPos, out hit)) {
+            // ... if it is not the player...
+            //Debug.DrawRay(checkPos, player.position + (Vector3.up * deltaPlayerHeight) - checkPos, Color.green);
+            if (hit.transform.gameObject.layer == 4) {
+                // This position isn't appropriate.
+                return false;
+            }
+        }
+        // If we haven't hit anything or we've hit the player, this is an appropriate position.
+        return true;
+    }
+
+    // Check for collision from player to camera.
+    private bool ReverseViewingPosCheck(Vector3 checkPos, float deltaPlayerHeight, float maxDistance) {
+        RaycastHit hit;
+
+        if (Physics.Raycast(player.position + (Vector3.up * deltaPlayerHeight), checkPos - player.position, out hit)) {
+            if (hit.transform != transform && hit.transform.gameObject.layer == collisionLayers) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- Assets/Scripts/character-controller/DraculaController.cs
+++ Assets/Scripts/character-controller/DraculaController.cs
@@ -26,7 +26,7 @@ public class DraculaController : MonoBehaviour {
             speedDirection += new Vector3(0.0f, 0.0f, 0.75f);
         }
         if (InputManager.instance.GetLeftStickDown()) {
-            speedDirection += new Vector3(0.0f, 0.0f, -0.5f);
+            speedDirection += new Vector3(0.0f, 0.0f, -0.1f);
         }
         if (InputManager.instance.GetLeftStickLeft()) {
             speedDirection += new Vector3(-0.5f, 0.0f, 0.0f);
