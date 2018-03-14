097745dab89475594703837e841438cd1aedcdbd
Marc Campins <marcampinscoll@gmail.com>
2018-03-14 18:00:50 +0100

    Create Prefab for UIRadialProgressBar

---
+++ Assets/Scripts/UI/RadialProgressBar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadialProgressBar : MonoBehaviour
+{
+    public Transform LoadingBar;
+    public Transform TextProgress;
+    [SerializeField] private float currentAmount;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentAmount = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentAmount < 0.0f)
+            currentAmount = 0.0f;
+
+        if (currentAmount > 100.0f)
+            currentAmount = 100.0f;
+
+        TextProgress.GetComponent<Text>().text = ((int)currentAmount).ToString() + "%";
+        LoadingBar.GetComponent<Image>().fillAmount = currentAmount / 100;
+    }
+
+    public void SetCurrentAmount(float amount)
+    {
+        currentAmount = amount;
+    }
+}
