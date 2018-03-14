8ce0f311df1cfc0265f582bb4bb39b3fc07bdd4e
Orphen <sergiramirezgarcia@gmail.com>
2018-03-14 20:47:07 +0100

    Trap attacks using bat attack

---
+++ Assets/Scripts/combat-system/AttackController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackController : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField]
+    private GameObject mainAttack;
+    [SerializeField]
+    private GameObject batAttack;
+
+	#endregion
+	
+	#region Properties
+	
+    #endregion
+	
+	#region MonoBehaviour Methods
+	
+	#endregion
+	
+	#region Public Methods
+	
+    public void InstantiateAttack(Transform enemy)
+    {
+        GameObject attack = (GameManager.instance.GetPlayer1().state == Player.PlayerStates.TURRET) ? batAttack : mainAttack;
+
+        GameObject attackClone = Instantiate(attack, transform.GetChild(0).position, transform.rotation);
+        attackClone.GetComponent<FollowTarget>().SetEnemy(enemy);
+    }
+
+	#endregion
+	
+	#region Private Methods
+	
+	#endregion
+}
