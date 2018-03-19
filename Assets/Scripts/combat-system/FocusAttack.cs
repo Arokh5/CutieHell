6fb524914aff267357ca52e0ddf9eedc1fd1ae32
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-03-19 02:41:56 +0100

    Fix multiple issues
    - Fix bug where enemies would keep a wrong material after being marked as target.
    - Fix issue where the Debug Canvas text color made it diffiicult to read.
    - Fix issue where enemies could be targetted throught walls.

--- Assets/Scripts/combat-system/FocusAttack.cs
+++ Assets/Scripts/combat-system/FocusAttack.cs
@@ -73,8 +73,15 @@ public class FocusAttack : MonoBehaviour
 
                 if (Physics.SphereCast(transform.position, sphereCastRadius, transform.forward, out hit, 100, layerMask.value))
                 {
-                    GameManager.instance.GetPlayer1().GetComponent<AttackController>().InstantiateAttack(hit.transform, hit.point);
-                    //Debug.Log("Hit enemy");
+                    if (hit.transform.GetComponent<AIEnemy>())
+                    {
+                        GameManager.instance.GetPlayer1().GetComponent<AttackController>().InstantiateAttack(hit.transform, hit.point);
+                        //Debug.Log("Hit enemy");
+                    }
+                    else
+                    {
+                        GameManager.instance.GetPlayer1().GetComponent<AttackController>().InstantiateAttack(null, Vector3.zero);
+                    }
                 }
                 else
                 {
