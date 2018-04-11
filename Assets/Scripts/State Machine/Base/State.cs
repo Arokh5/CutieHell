06f88b3663c72a93752cfbd3ac227ffdfe8b6fda
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-04-11 22:04:50 +0200

    Implement the base classes for a ScriptableObject-based State Machine system

---
+++ Assets/Scripts/State Machine/Base/State.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu (menuName ="State Machine/State")]
+public class State : ScriptableObject
+{
+    #region Fields
+    public StateAction onEnterAction;
+    public StateAction onExitAction;
+    public StateAction[] stateActions;
+    public Transition[] transitions;
+    #endregion
+
+    #region Public Methods
+    public void UpdateState(StateMachineController controller)
+    {
+        foreach (StateAction action in stateActions)
+        {
+            action.Act(controller);
+        }
+
+        foreach(Transition transition in  transitions)
+        {
+            if (transition.decision.Decide(controller))
+            {
+                controller.TransitionToState(transition.targetState);
+                break;
+            }
+        }
+    }
+    #endregion
+}
---
+++ Assets/Scripts/State Machine/Base/StateMachineController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class StateMachineController : MonoBehaviour {
+
+    [SerializeField]
+    protected State currentState;
+
+    public virtual void TransitionToState(State targetState)
+    {
+        if (currentState.onExitAction)
+            currentState.onExitAction.Act(this);
+
+        if (targetState.onEnterAction)
+            targetState.onEnterAction.Act(this);
+
+        currentState = targetState;        
+    }
+}
