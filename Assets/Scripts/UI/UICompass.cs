abdbfa13924805c2f1db689066144178891cfe5b
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-04-17 14:44:16 +0200

    Implement the basis of the compass system
    - Create the UICompass and the CompassIcon classes.
    - Include 3 testing icons (for Monument, Traps and Spawners)

---
+++ Assets/Scripts/UI/UICompass.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICompass : MonoBehaviour {
+
+    #region Fields
+    public Transform referenceTransform;
+    [Range(60, 180)]
+    public float fovDegrees = 90.0f;
+    public CompassIcon[] compassIcons;
+
+    private RectTransform rectTransform;
+    private Vector2 size;
+    #endregion
+
+    #region MonoBehaviour Methods
+    private void Awake()
+    {
+        if (compassIcons == null || compassIcons.Length == 0)
+        {
+            gameObject.SetActive(false);
+            Debug.LogWarning("WARNING: The UICompass in gameObject '" + gameObject.name + "' doesn't have any compassIcons assigned so it will be deactivated!");
+        }
+
+        if(rectTransform == null)
+        {
+            rectTransform = GetComponent<RectTransform>();
+            UnityEngine.Assertions.Assert.IsNotNull(rectTransform, "ERROR: The UICompass in gameObject '" + gameObject.name + "' doesn't have a RectTransform attached!");
+        }
+    }
+
+    private void Start()
+    {
+        size = rectTransform.sizeDelta;
+    }
+
+    private void Update () {
+		
+	}
+    #endregion
+
+    #region Public Methods
+
+    #endregion
+}
