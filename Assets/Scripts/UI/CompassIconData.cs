2e0584fc2de16657de73a0b66abd5a9f3c1dfa8e
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-04-17 15:52:30 +0200

    Set up the tracking behaviour of the Compass
    - Rename the CompassIcon class to CompassIconData

---
+++ Assets/Scripts/UI/CompassIconData.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class CompassIconData {
+    public Sprite iconSprite;
+    public Transform transform;
+}
--- Assets/Scripts/UI/UICompass.cs
+++ Assets/Scripts/UI/UICompass.cs
@@ -7,18 +7,20 @@ public class UICompass : MonoBehaviour {
 
     #region Fields
     public Transform referenceTransform;
+    public RectTransform iconImagePrefab;
     [Range(60, 180)]
     public float fovDegrees = 90.0f;
-    public CompassIcon[] compassIcons;
+    public CompassIconData[] compassIconsData;
 
     private RectTransform rectTransform;
+    private RectTransform[] iconsRectTranforms;
     private Vector2 size;
     #endregion
 
     #region MonoBehaviour Methods
     private void Awake()
     {
-        if (compassIcons == null || compassIcons.Length == 0)
+        if (compassIconsData == null || compassIconsData.Length == 0)
         {
             gameObject.SetActive(false);
             Debug.LogWarning("WARNING: The UICompass in gameObject '" + gameObject.name + "' doesn't have any compassIcons assigned so it will be deactivated!");
@@ -34,14 +36,45 @@ public class UICompass : MonoBehaviour {
     private void Start()
     {
         size = rectTransform.sizeDelta;
+
+        // Create the spawnerIcon images
+        iconsRectTranforms = new RectTransform[compassIconsData.Length];
+        for (int i = 0; i < iconsRectTranforms.Length; ++i)
+        {
+            iconsRectTranforms[i] = Instantiate(iconImagePrefab, gameObject.transform, false);
+            Image image = iconsRectTranforms[i].GetComponent<Image>();
+            image.sprite = compassIconsData[i].iconSprite;
+            //iconsRectTranforms[i].gameObject.SetActive(false);
+        }
     }
 
     private void Update () {
-		
+        UpdateIconsPosition();
 	}
     #endregion
 
-    #region Public Methods
+    #region Private Methods
+    private void UpdateIconsPosition()
+    {
+        Vector2 referenceForward = new Vector2(referenceTransform.forward.x, referenceTransform.forward.z);
 
+        for (int i = 0; i < compassIconsData.Length; ++i)
+        {
+            Vector3 referenceToIconV3 = compassIconsData[i].transform.position - referenceTransform.position;
+            Vector2 referenceToIcon = new Vector2(referenceToIconV3.x, referenceToIconV3.z);
+            float angle = -Vector2.SignedAngle(referenceForward, referenceToIcon);
+
+            if (Mathf.Abs(angle) > 0.5f * fovDegrees)
+            {
+                iconsRectTranforms[i].gameObject.SetActive(false);
+            }
+            else
+            {
+                iconsRectTranforms[i].gameObject.SetActive(true);
+                float xPos = angle / (0.5f * fovDegrees) * 0.5f * size.x;
+                iconsRectTranforms[i].localPosition = xPos * Vector3.right;
+            }
+        }
+    }
     #endregion
 }
