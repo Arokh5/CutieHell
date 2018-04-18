f50677038df53c76ee90526dce07919e164428e9
Orphen <sergiramirezgarcia@gmail.com>
2018-04-18 10:43:45 +0200

    Make logic of strong attack x5 combo

--- Assets/Scripts/combat-system/StrongAttackBehaviour.cs
+++ Assets/Scripts/combat-system/StrongAttackBehaviour.cs
@@ -9,12 +9,14 @@ public class StrongAttackBehaviour : MonoBehaviour {
     public StrongAttackExplosionDetection enemiesList;
     private float timer;
     private float timerToDisable;
-
+    private const int enemiesToCombo = 5;
+    private int comboCount;
 
     private void OnEnable () {
         enemiesList.currentStrongAttackTargets.Clear();
         timer = 0.5f;
         timerToDisable = 2.8f;
+        comboCount = 0;
 	}
 	
 	void Update () {
@@ -37,7 +39,11 @@ public class StrongAttackBehaviour : MonoBehaviour {
         {
             aiEnemy.MarkAsTarget(false);
             aiEnemy.TakeDamage(damage, AttackType.STRONG);
+            comboCount++;
         }
+
+        if (comboCount >= enemiesToCombo)
+            Debug.Log("COMBO!!");
 
         enemiesList.currentStrongAttackTargets.Clear();
         GameManager.instance.GetPlayer1().timeSinceLastStrongAttack = 0f;
