1b6e5367fd18fef719e580ebac5eb61f49af1f1e
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-04-21 11:38:12 +0200

    Remove monument-repair test scene and fix issue where using a trap would grant the Player evil instead of taking it away

--- Assets/Scripts/Player State Machine/Actions/Entry Actions/TrapEnterAction.cs
+++ Assets/Scripts/Player State Machine/Actions/Entry Actions/TrapEnterAction.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Player State Machine/Actions/TrapEnterAction")]
 public class TrapEnterAction : StateAction
 {
-    public int evilCost = 10;
-
     public override void Act(Player player)
     {
         player.currentTrap = player.nearbyTrap;
         player.currentTrap.Activate(player);
         player.SetRenderersVisibility(false);
-        player.SetEvilLevel(player.currentTrap.usageCost);
+        player.SetEvilLevel(-player.currentTrap.usageCost);
 
         player.bulletSpawnPoint.SetParent(player.nearbyTrap.transform);
         player.bulletSpawnPoint.localPosition = new Vector3(0f, 0.3f, 0.7f);
