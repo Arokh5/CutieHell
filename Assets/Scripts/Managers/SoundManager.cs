c48d1774a03efc072d2a048ba32dbf69dd926760
Orphen <sergiramirezgarcia@gmail.com>
2018-04-22 17:06:09 +0200

    Add SoundManager and create prefabs for GameManager and SoundManager

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    #region Fields
+
+    public static SoundManager instance;
+
+    [SerializeField]
+    private AudioSource musicSource;
+    [SerializeField]
+    private AudioSource efxSource;
+
+	#endregion
+	
+	#region MonoBehaviour Methods
+	
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+
+	#endregion
+	
+	#region Public Methods
+	
+    public void PlayMusicClip(AudioClip musicClip)
+    {
+        musicSource.clip = musicClip;
+        musicSource.Play();
+    }
+
+    public void PlayEfxClip(AudioClip efxClip)
+    {
+        efxSource.clip = efxClip;
+        efxSource.Play();
+    }
+
+	#endregion
+}
