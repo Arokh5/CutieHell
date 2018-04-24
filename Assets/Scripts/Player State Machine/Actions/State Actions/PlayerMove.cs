18b0a9a0a96368f2fc3723042e5f868533263e1e
Marc Campins <marcampinscoll@gmail.com>
2018-04-24 12:06:48 +0200

    Set speed of movement relative to how much you move the left stick, deny movement when speed is too slow, and add angular drag in order to break correctly and not slide

--- Assets/Scripts/Player State Machine/Actions/State Actions/PlayerMove.cs
+++ Assets/Scripts/Player State Machine/Actions/State Actions/PlayerMove.cs
@@ -10,33 +10,36 @@ public class PlayerMove : StateAction
     public override void Act(Player player)
     {
         Vector3 speedDirection = Vector3.zero;
+        Vector3 verticalSpeed = new Vector3(0f, 0f, 0.5f);
+        Vector3 horizontalSpeed = new Vector3(0.5f, 0f, 0f);
 
         if (InputManager.instance.GetLeftStickUp())
         {
-            speedDirection += new Vector3(0.0f, 0.0f, 0.5f);
+            speedDirection += verticalSpeed * -InputManager.instance.GetLeftStickUpValue();
         }
         if (InputManager.instance.GetLeftStickDown())
         {
-            speedDirection += new Vector3(0.0f, 0.0f, -0.5f);
+            speedDirection += verticalSpeed * -InputManager.instance.GetLeftStickDownValue();
         }
         if (InputManager.instance.GetLeftStickLeft())
         {
-            speedDirection += new Vector3(-0.5f, 0.0f, 0.0f);
+            speedDirection += horizontalSpeed * InputManager.instance.GetLeftStickLeftValue();
         }
         if (InputManager.instance.GetLeftStickRight())
         {
-            speedDirection += new Vector3(0.5f, 0.0f, 0.0f);
+            speedDirection += horizontalSpeed * InputManager.instance.GetLeftStickRightValue();
         }
-
-        if (speedDirection.magnitude > 0.0f)
+        
+        if (speedDirection.magnitude > 0.2f)
         {
-            player.rb.drag = 0.0f;
+            player.rb.drag = 0.5f;
             player.footSteps.SetActive(true);
             player.animator.SetBool("Move", true);
         }
         else
         {
-            player.rb.drag = 10.0f;
+            player.rb.drag = 10f;
+            player.rb.angularDrag = 10f;
             player.footSteps.SetActive(false);
             player.animator.SetBool("Move", false);
         }
