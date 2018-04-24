593f1100c100a44b1da9374456fb33bd7d86ccb5
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-04-24 14:24:20 +0200

    Modify the Conqueror behaviour so that it will keep moving forward even when getting hit

--- Assets/Scripts/AnimatorStateMachineBehaivour/GetHitConquerorAnimatorBehaivour.cs
+++ Assets/Scripts/AnimatorStateMachineBehaivour/GetHitConquerorAnimatorBehaivour.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class GetHitConquerorAnimatorBehaivour : StateMachineBehaviour {
-
+    
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
