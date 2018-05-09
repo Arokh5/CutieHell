939de0ef6c650a929a09a76fa75da1b3675313c7
Marc Campins <marcampinscoll@gmail.com>
2018-05-09 20:48:18 +0200

    Objective marker implemented, must keep in mind that it is now an approximation (magical number used)

--- Assets/ObjectiveMarker.cs
+++ Assets/ObjectiveMarker.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class ObjectiveMarker : MonoBehaviour
 {
 
-    public float horizontalOffsetPercentage = 0.15f;
-    public float verticalOffsetPercentage = 0.25f;
+    public float horizontalOffsetPercentage = 0.05f;
+    public float bottomOffset = 0.1f;
+    public float topOffset = 0.7f;
     public Transform target;
     [SerializeField]
     private Camera mainCamera;
@@ -14,64 +15,74 @@ public class ObjectiveMarker : MonoBehaviour
     private float horizontalOffset;
     private float verticalOffset;
     private bool front;
-    private float exitHeight;
-    private float downwardsDistance;
+    private float heightLimit;
+    private float upwardsDistance;
     private float startingDepth;
 
     void Start()
     {
-        horizontalOffset = (mainCamera.pixelWidth / 2) * horizontalOffsetPercentage;
-        verticalOffset = (mainCamera.pixelHeight / 2) * verticalOffsetPercentage;
-        exitHeight = 0f;
+        horizontalOffset = mainCamera.pixelWidth * horizontalOffsetPercentage;
+        verticalOffset = mainCamera.pixelHeight * bottomOffset;
+        startingDepth = 0f;
     }
 
     void Update()
     {
-        Vector3 screenPos = transform.position;
-        screenPos = mainCamera.WorldToScreenPoint(target.position);
+        Vector3 screenPos = mainCamera.WorldToScreenPoint(target.position);
+        float distance = Vector3.Distance(mainCamera.transform.position, target.transform.position);
 
         front = screenPos.z >= 0f;
-        
+
         if (front)
         {
+            /* Check if objective marker is out of bounds */
+            bool exit = false;
             if (screenPos.x < horizontalOffset)
             {
-                if (exitHeight == 0f)
-                {
-                    exitHeight = mainCamera.pixelHeight * 0.5f;
-                    downwardsDistance = (exitHeight - verticalOffset) / screenPos.z;
-                    startingDepth = screenPos.z;
-                }
-
-                screenPos.y = exitHeight;
-
                 screenPos.x = horizontalOffset;
-                screenPos.y -= downwardsDistance * (startingDepth - screenPos.z);
+                if (startingDepth == 0f)
+                  startingDepth = screenPos.z;
+                exit = true;
             }
-            else if (screenPos.x > mainCamera.pixelWidth - horizontalOffset)
+            if (screenPos.x > mainCamera.pixelWidth - horizontalOffset)
             {
-                if (exitHeight == 0f)
-                {
-                    exitHeight = mainCamera.pixelHeight * 0.5f;
-                    downwardsDistance = (exitHeight - verticalOffset) / screenPos.z;
-                    startingDepth = screenPos.z;
-                }
-
-                screenPos.y = exitHeight;
-
                 screenPos.x = mainCamera.pixelWidth - horizontalOffset;
-                screenPos.y -= downwardsDistance * (startingDepth - screenPos.z);
+                if (startingDepth == 0f)
+                    startingDepth = screenPos.z;
+                exit = true;
+            }
+
+            /* If not out of bounds just limit vertical offset */
+            if (!exit)
+            {
+                if (screenPos.y > mainCamera.pixelHeight * topOffset)
+                    screenPos.y = mainCamera.pixelHeight * topOffset;
+
+                if (screenPos.y < verticalOffset)
+                    screenPos.y = verticalOffset;
+
+                startingDepth = 0f;
             }
             else
+            /* Else, adjust the vertical coordinate of the marker */
             {
-                if (screenPos.y > mainCamera.pixelHeight * 0.7f)
-                    screenPos.y = mainCamera.pixelHeight * 0.7f;
+                heightLimit = screenPos.y;
 
-                exitHeight = 0f;
+                if (heightLimit > mainCamera.pixelHeight * topOffset)
+                    heightLimit = mainCamera.pixelHeight * topOffset;
+
+                if (heightLimit < verticalOffset)
+                    heightLimit = verticalOffset;
+
+                upwardsDistance = (heightLimit - verticalOffset);
+
+                /* Magical number */
+                screenPos.y = verticalOffset + (upwardsDistance * (screenPos.z / (distance * 0.75f)));
             }
         }
         else
         {
+            /* If not on front switch horizontal coordinate */
             screenPos.x = mainCamera.pixelWidth - screenPos.x;
 
             if (screenPos.x < horizontalOffset)
@@ -83,6 +94,7 @@ public class ObjectiveMarker : MonoBehaviour
                 screenPos.x = mainCamera.pixelWidth - horizontalOffset;
             }
 
+            /* Stick vertical coordinate to bottom of screen */
             screenPos.y = verticalOffset;
         }
 
