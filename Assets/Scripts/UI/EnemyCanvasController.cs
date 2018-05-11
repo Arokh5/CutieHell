7a546c34f628441dc1fdea7b71871c94aa2c3a0d
Orphen <sergiramirezgarcia@gmail.com>
2018-05-11 20:14:54 +0200

    Make health bar dissapear with a fade out effect

--- Assets/Scripts/UI/EnemyCanvasController.cs
+++ Assets/Scripts/UI/EnemyCanvasController.cs
@@ -11,9 +11,13 @@ public class EnemyCanvasController : MonoBehaviour
     private float damageActiveTime;
     [SerializeField]
     private float targetActiveTime;
+    [SerializeField]
+    private float fadeOutBaseTime;
+    private float fadeOutTime;
     private Image healthImage;
     private float baseHealth;
     private float time;
+    private bool fadeOut;
 
 	#endregion
 	
@@ -21,6 +25,8 @@ public class EnemyCanvasController : MonoBehaviour
 
     private void Awake()
     {
+        fadeOut = false;
+        fadeOutTime = fadeOutBaseTime;
         healthImage = healthBarCanvas.transform.GetChild(0).GetComponent<Image>();
         baseHealth = GetComponent<AIEnemy>().baseHealth;
     }
@@ -29,9 +35,14 @@ public class EnemyCanvasController : MonoBehaviour
     {
         healthBarCanvas.GetComponent<RectTransform>().LookAt(GameManager.instance.GetPlayer1().transform);
 
-        if (healthBarCanvas.activeSelf)
+        if (healthBarCanvas.activeSelf && !fadeOut)
         {
             DisableHealthBar();
+        }
+
+        if (fadeOut)
+        {
+            HealthBarFadeOut();
         }
     }
 
@@ -47,6 +58,8 @@ public class EnemyCanvasController : MonoBehaviour
     public void EnableHealthBar(bool takeDamage)
     {
         healthBarCanvas.SetActive(true);
+        healthBarCanvas.transform.GetChild(0).GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+        healthBarCanvas.transform.GetChild(1).GetComponent<Image>().color = new Color32(255, 255, 255, 255);
 
         if (takeDamage || (!takeDamage && time <= targetActiveTime))
         {
@@ -64,6 +77,20 @@ public class EnemyCanvasController : MonoBehaviour
 
         if (time <= 0f)
         {
+            fadeOut = true;
+        }
+    }
+
+    private void HealthBarFadeOut()
+    {
+        fadeOutTime -= Time.deltaTime;
+        healthBarCanvas.transform.GetChild(0).GetComponent<Image>().color = Color.Lerp(new Color32(255, 255, 255, 255), new Color32(255, 255, 255, 0), 1 - (fadeOutTime / fadeOutBaseTime));
+        healthBarCanvas.transform.GetChild(1).GetComponent<Image>().color = Color.Lerp(new Color32(255, 255, 255, 255), new Color32(255, 255, 255, 0), 1 - (fadeOutTime / fadeOutBaseTime));
+
+        if (fadeOutTime <= 0)
+        {
+            fadeOut = false;
+            fadeOutTime = fadeOutBaseTime;
             healthBarCanvas.SetActive(false);
         }
     }
