22bbfd07cad15fe69411c90e100a55d2d73c9a84
Marc Campins <marcampinscoll@gmail.com>
2018-05-14 15:13:41 +0200

    Add arrow pointing towards the direction the player must turn to find objective

--- Assets/Scripts/UI/ObjectiveMarker.cs
+++ Assets/Scripts/UI/ObjectiveMarker.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ObjectiveMarker : MonoBehaviour
 {
-
     public float horizontalOffsetPercentage = 0.05f;
     public float bottomOffset = 0.1f;
     public float topOffset = 0.7f;
     public Transform target;
+    public Image arrow;
     [SerializeField]
     private Camera mainCamera;
 
@@ -18,16 +19,21 @@ public class ObjectiveMarker : MonoBehaviour
     private float heightLimit;
     private float upwardsDistance;
     private float startingDepth;
+    private float previousRotation;
 
     void Start()
     {
         horizontalOffset = mainCamera.pixelWidth * horizontalOffsetPercentage;
         verticalOffset = mainCamera.pixelHeight * bottomOffset;
         startingDepth = 0f;
+        /* Get arrow to point up */
+        previousRotation = 0f;
     }
 
     void Update()
     {
+        arrow.gameObject.SetActive(true);
+
         Vector3 screenPos = mainCamera.WorldToScreenPoint(target.position);
         float distance = Vector3.Distance(mainCamera.transform.position, target.transform.position);
 
@@ -36,32 +42,41 @@ public class ObjectiveMarker : MonoBehaviour
         if (front)
         {
             /* Check if objective marker is out of bounds */
-            bool exit = false;
+            bool outOfBounds = false;
             if (screenPos.x < horizontalOffset)
             {
                 screenPos.x = horizontalOffset;
                 if (startingDepth == 0f)
-                  startingDepth = screenPos.z;
-                exit = true;
+                    startingDepth = screenPos.z;
+                outOfBounds = true;
             }
             if (screenPos.x > mainCamera.pixelWidth - horizontalOffset)
             {
                 screenPos.x = mainCamera.pixelWidth - horizontalOffset;
                 if (startingDepth == 0f)
                     startingDepth = screenPos.z;
-                exit = true;
+                outOfBounds = true;
             }
 
             /* If not out of bounds just limit vertical offset */
-            if (!exit)
+            if (!outOfBounds)
             {
                 if (screenPos.y > mainCamera.pixelHeight * topOffset)
+                {
                     screenPos.y = mainCamera.pixelHeight * topOffset;
+                    outOfBounds = true;
+                }
 
                 if (screenPos.y < verticalOffset)
+                {
                     screenPos.y = verticalOffset;
+                    outOfBounds = true;
+                }
 
                 startingDepth = 0f;
+
+                if (!outOfBounds)
+                    arrow.gameObject.SetActive(false);
             }
             else
             /* Else, adjust the vertical coordinate of the marker */
@@ -78,6 +93,7 @@ public class ObjectiveMarker : MonoBehaviour
 
                 /* Magical number */
                 screenPos.y = verticalOffset + (upwardsDistance * (screenPos.z / (distance * 0.75f)));
+
             }
         }
         else
@@ -98,6 +114,25 @@ public class ObjectiveMarker : MonoBehaviour
             screenPos.y = verticalOffset;
         }
 
-        transform.position = screenPos;
+        RectTransform iconAnchor = gameObject.GetComponent<Image>().rectTransform;
+        RectTransform arrowAnchor = arrow.rectTransform;
+
+        iconAnchor.position = screenPos;
+
+        /* Get angle between straight up vector (initial arrow position) and current center of screen */
+        Vector3 centerOfScreen = mainCamera.ViewportToScreenPoint(new Vector3(0.5f, 0.5f, 0f));
+        Vector3 centerOfIcon = transform.position;
+        centerOfIcon.z = 0f;
+
+        float angle = Vector3.SignedAngle(new Vector3(0f, 1f, 0f), centerOfScreen - centerOfIcon, new Vector3(0f, 0f, 1f));
+        Vector3 arrowRotation = arrowAnchor.parent.localRotation.eulerAngles;
+        arrowRotation.z = angle;
+        arrowAnchor.parent.localRotation = Quaternion.Euler(arrowRotation);
+        //if (Mathf.Abs(previousRotation - angle) > float.Epsilon)
+        //{
+        //    arrow.transform.parent.Rotate(new Vector3(0f, 0f, -previousRotation));
+        //    arrow.transform.parent.Rotate(new Vector3(0f, 0f, angle));
+        //    previousRotation = angle;
+        //}
     }
 }
