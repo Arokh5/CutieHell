12d8cbee9a0ae612a618cab4b64e2d6615ab1820
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-05-28 15:41:33 +0200

    Modify the EventLauncher to be used with Physics Triggers
    - Use an EventLauncher (Trigger) to remove control from the Player after allowing some motion.

---
+++ Assets/Scripts/Player tutorial State Machine/Exit Action/StopMoveAnimation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Player Tutorial State Machine/Actions/StopMoveAnimation")]
+public class StopMoveAnimation : StateAction
+{
+    public override void Act(Player player)
+    {
+        player.animator.SetBool("Move", false);
+        player.footSteps.SetActive(false);
+        player.footstepsSource.Stop();
+    }
+}
--- Assets/Scripts/Tutorial/EventLauncher.cs
+++ Assets/Scripts/Tutorial/EventLauncher.cs
@@ -2,8 +2,20 @@
 
 public class EventLauncher : MonoBehaviour
 {
+    private enum LauncherType
+    {
+        ACTIVATION,
+        TRIGGER_ENTER,
+        TRIGGER_EXIT
+    }
+
     #region Fields
+    [SerializeField]
+    private LauncherType type;
     public int eventIndex = -1;
+    public bool deactivateOnLaunch = false;
+    public LayerMask triggerLayerMask;
+
     private TutorialController tutorialController;
     #endregion
 
@@ -12,13 +24,43 @@ public class EventLauncher : MonoBehaviour
     {
         tutorialController = GetComponentInParent<TutorialController>();
         UnityEngine.Assertions.Assert.IsNotNull(tutorialController, "ERROR: A TutorialController Component could not be found in the parent hierarchy by EventLauncher in GameObject " + gameObject.name);
-        gameObject.SetActive(false);
     }
 
     private void OnEnable()
     {
-        if (tutorialController)
-            tutorialController.LaunchEvent(eventIndex);
+        if (type == LauncherType.ACTIVATION && tutorialController)
+            LaunchEvent();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (type == LauncherType.TRIGGER_ENTER && tutorialController)
+        {
+            if (triggerLayerMask.value == other.gameObject.layer)
+            {
+                LaunchEvent();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (type == LauncherType.TRIGGER_EXIT && tutorialController)
+        {
+            if (((1 << other.gameObject.layer) & triggerLayerMask) != 0)
+            {
+                LaunchEvent();
+            }
+        }
+    }
+    #endregion
+
+    #region Private Moethods
+    private void LaunchEvent()
+    {
+        tutorialController.LaunchEvent(eventIndex);
+        if (deactivateOnLaunch)
+            gameObject.SetActive(false);
     }
     #endregion
 }
