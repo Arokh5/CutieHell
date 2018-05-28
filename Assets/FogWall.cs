ece8ef7acb78ba34687221a9139faff69ca8d7e9
Dani Reyes <dani-kingdom@hotmail.com>
2018-05-28 22:54:16 +0200

    Add fog walls

---
+++ Assets/FogWall.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogWall : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == 8)
+        {
+            other.GetComponent<AIEnemy>().TakeDamage(200, AttackType.NONE);
+        }
+    }
+}
---
+++ Assets/LivingFogManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivingFogManager : MonoBehaviour {
+
+    [SerializeField]
+    private FogWall[] fogWalls;
+    [SerializeField]
+    private GameObject[] deactivatedVFX;
+
+    void Start () {
+		
+	}
+
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            for(int i = 0; i < fogWalls.Length; i++)
+            {
+                DeactivateFogWall(i);
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            for (int i = 0; i < fogWalls.Length; i++)
+            {
+                ActivateFogWall(i);
+            }
+        }
+	}
+
+    public void DeactivateFogWall(int index)
+    {
+        fogWalls[index].gameObject.SetActive(false);
+        deactivatedVFX[index].SetActive(true);
+    }
+    public void ActivateFogWall(int index)
+    {
+        fogWalls[index].gameObject.SetActive(true);
+        deactivatedVFX[index].SetActive(false);
+    }
+}
