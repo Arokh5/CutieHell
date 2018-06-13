fe57bea17684182c03b5dcee398a5b401b6a4896
Mac092 <m.rodmur@gmail.com>
2018-06-13 09:32:58 +0200

    Update Evil UI System to show only one frame, which different filling colors to represent every evil point Vlad has

--- Assets/EvilManaController.cs
+++ Assets/EvilManaController.cs
@@ -1,79 +1,78 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+
 public class EvilManaController : MonoBehaviour {
 
+
     #region Attributes
-    [Header("Frames")]
+    [Header("Fragments UI info")]
     [SerializeField]
-    private EvilFrameController evilWeakFrame;
+    private Image evilFragmentsFiller;
     [SerializeField]
-    private EvilFrameController evilStrongFrame;
+    private Text evilFragmentsNumber;
+
+    [Header("Colors for the evil Circle")]
+    [SerializeField]
+    Color[] colors;
 
     private float maxPlayerEvil = 0;
-    private bool isMainFrameActive = true;
+    private float evilUnits = 0;
     #endregion
 
     #region MonoBehaviour Methods
     // Use this for initialization
     void Start()
     {
-        UnityEngine.Assertions.Assert.IsNotNull(evilWeakFrame, "Error : evilWeakFrame is not assigned in GameObject: " + gameObject.name);
-        UnityEngine.Assertions.Assert.IsNotNull(evilStrongFrame, "Error: evilStrongFrame is not assigned in GameObject: " + gameObject.name);
-
+        UnityEngine.Assertions.Assert.IsNotNull(evilFragmentsNumber, "Error: No Text assigned to: " + gameObject.name);
+        
         maxPlayerEvil = GameManager.instance.GetPlayer1().GetMaxEvilLevel();
-    }
+        UnityEngine.Assertions.Assert.AreEqual(maxPlayerEvil / 10, colors.Length, "Error: Player's max evil level attribute and and numEvilColors must match" + gameObject.name);
 
-    // Update is called once per frame
-    void Update()
-    {
+        evilUnits = NormalizeEvilNumber(maxPlayerEvil);
 
+        evilFragmentsNumber.text = NormalizeEvilNumber(maxPlayerEvil).ToString("0");
+        
     }
     #endregion
 
     #region Public methods
-    public void SetMainFrameActive(bool active)
-    {
-        isMainFrameActive = active;
-    }
-
-    public bool GetMainFrameActive()
-    {
-        return isMainFrameActive;
-    }
 
     public void UpdateCurrentEvil(float currentEvil)
     {
-        if (isMainFrameActive)
+        Debug.Log(currentEvil);
+        evilFragmentsFiller.fillAmount = ( (float) currentEvil/100);
+        Debug.Log(evilFragmentsFiller.fillAmount);
+        
+        if (int.Parse(evilFragmentsNumber.text) != NormalizeEvilNumber(currentEvil))
         {
-            evilStrongFrame.UpdateFragmentsFiller(currentEvil);
-
-            //Check if mainFrame has to be deactivated
-            if (currentEvil <= maxPlayerEvil / 2)
-            {
-                evilStrongFrame.gameObject.SetActive(false);
-                isMainFrameActive = false;
-
-                evilWeakFrame.UpdateFragmentsFiller(currentEvil);
-            }
+            float normalizedEvil = NormalizeEvilNumber(currentEvil);
+            evilFragmentsNumber.text = normalizedEvil.ToString();
         }
-        else
-        {
-            evilWeakFrame.UpdateFragmentsFiller(currentEvil);
 
-            //Check if mainFrame has to be activated
-            if (currentEvil > maxPlayerEvil / 2)
-            {
-                evilStrongFrame.gameObject.SetActive(true);
-                isMainFrameActive = true;
-
-                evilStrongFrame.UpdateFragmentsFiller(currentEvil);
-            }
-        }
     }
     #endregion
 
     #region Private methods
+    private int NormalizeEvilNumber(float evil)
+    {
+        int normalizedEvilNumber = 0;
+        //Divided / 10 because we are working two digits evil values, but the UI shows only one digit value. 
+        normalizedEvilNumber = (int)(evil / 10);
 
+        return normalizedEvilNumber;
+    }
+
+    private void ColorCircleFiller(int currentEvil)
+    {
+        for (int i = 0; i < colors.Length; i++)
+        {
+            if(i == currentEvil)
+            {
+                evilFragmentsFiller.color = colors[i];
+                break;
+            }
+        }
+    }
     #endregion
 }
