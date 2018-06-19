6b8682fc288206cc2513a4681074fd7711873371
Mac092 <m.rodmur@gmail.com>
2018-06-19 15:07:34 +0200

    Set up new mana initialization

---
+++ Assets/Scripts/UI/EvilManaController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class EvilManaController : MonoBehaviour {
+
+    #region Attributes
+
+    [Header("Evil UI")]
+    [SerializeField]
+    private EvilPointController[] evilPoints;
+    private float maxEvil;
+	#endregion
+	
+	#region MonoBehaviour methods
+	// Use this for initialization
+	void Start () 
+	{
+        maxEvil = GameManager.instance.GetPlayer1().GetMaxEvilLevel();
+        UnityEngine.Assertions.Assert.AreEqual(maxEvil, evilPoints.Length, "ERROR: There has to be as many UI EvilPoints as Player maxEvil value, check GameObject called: " + gameObject.name);
+        InitializeEvilPoints();
+    }
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	#endregion
+	
+	
+	
+	#region Public methods
+	
+	#endregion
+	
+	#region Private methods
+	void InitializeEvilPoints()
+    {
+        for(int i = 0; i < evilPoints.Length; i++)
+        {
+            evilPoints[i].completeFill();
+        }
+    } 
+	#endregion
+}
---
+++ Assets/Scripts/UI/EvilPointController.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EvilPointController : MonoBehaviour {
+
+    enum EvilPointState { FULL, VOID, CHARGING }
+
+    #region Attributes
+    private EvilPointState evilPointState;
+    private float evilCurrentAmount;
+
+    [Header("UI Elements")]
+    [SerializeField]
+    private Image evilBarFillness;
+    [SerializeField]
+    private Image evilBarBase;
+    [SerializeField]
+    private Image evilBarIcon;
+    [SerializeField]
+    private Image evilBarProgressMarker;
+    #endregion
+
+    #region MonoBehaviour methods
+    // Use this for initialization
+    void Start()
+    {
+        UnityEngine.Assertions.Assert.IsNotNull(evilBarFillness, "ERROR: No evilBarFillnesS set up in gameobject" + gameObject.name);
+        UnityEngine.Assertions.Assert.IsNotNull(evilBarBase, "ERROR: No evilBarBase set up in gameobject" + gameObject.name);
+        UnityEngine.Assertions.Assert.IsNotNull(evilBarIcon, "ERROR: No evilBarIcon set up in gameobject" + gameObject.name);
+        UnityEngine.Assertions.Assert.IsNotNull(evilBarProgressMarker, "ERROR: No evilBarProgressMarker set up in gameobject" + gameObject.name);
+        evilCurrentAmount = GameManager.instance.GetPlayer1().GetMaxEvilLevel();
+        evilPointState = EvilPointState.FULL;
+
+    }
+
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    #region Public methods
+
+    public void ModifyEvilPoint(float amount)
+    {
+        evilCurrentAmount = amount;
+        if(evilCurrentAmount == 1)
+        {
+
+        }else if(evilCurrentAmount == 0)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    public void completeFill()
+    {
+        Color transparentMarker = evilBarProgressMarker.color;
+        transparentMarker.a = 0f;
+        evilBarProgressMarker.color = transparentMarker;
+
+        evilBarIcon.color = Color.white;
+
+        evilBarFillness.fillAmount = 1f;
+
+        evilPointState = EvilPointState.FULL;
+    }
+
+    public void completeVoid()
+    {
+        Color opaqueMarker = evilBarProgressMarker.color;
+        opaqueMarker.a = 1f;
+        evilBarProgressMarker.color = opaqueMarker;
+
+        evilBarIcon.color = Color.grey;
+
+        evilPointState = EvilPointState.VOID;
+    }
+    #endregion
+
+    #region Private methods
+
+    #endregion
+}
