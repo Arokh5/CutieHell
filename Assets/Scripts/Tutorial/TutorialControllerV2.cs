2f01d35341c3bab5fae813667becae85d1578605
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-06-23 14:36:32 +0200

    Modify the tutorial GameObject structure in preparation for the new cinematic

---
+++ Assets/Scripts/Tutorial/TutorialControllerV2.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialControllerV2 : TutorialController
+{
+    #region Fields
+
+    #endregion
+
+    #region Public Methods
+    public override int GetEnemiesCount()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override bool IsRunning()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void LaunchEvent(int eventIndex)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void NextPlayerState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void PauseTimelineAndReleaseCamera()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void PauseTutorial(bool pause)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void RequestBypassTutorial()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void RequestEndTutorial()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void RequestStartTutorial()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ResumeTimelineAndCaptureCamera()
+    {
+        throw new System.NotImplementedException();
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Tutorial/TutorialEventsV2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialEventsV2 : TutorialEvents
+{
+    #region Fields
+
+    #endregion
+
+    #region Public Methods
+    public override int GetEnemiesCount()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void LaunchEvent(int eventIndex)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnTutorialEnded()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnTutorialStarted()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetTutorialEnemiesManager(TutorialEnemiesManager tutorialEnemiesManager)
+    {
+        throw new System.NotImplementedException();
+    }
+    #endregion
+}
