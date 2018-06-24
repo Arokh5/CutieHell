702b4d15119765dd107fe322d0aefcad5be04820
Mac092 <m.rodmur@gmail.com>
2018-06-24 22:46:28 +0200

    Create Combo base class, from now on combos will inherit from this parent class and will be connected to the StatsManager class

---
+++ Assets/Scripts/General/Combos/Combo.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class Combo : MonoBehaviour
+{
+
+    #region Attributes
+    public string comboName;
+    public string description;
+    public int reward;
+    protected int currentCount = 0;
+    protected bool comboEnabled = false;
+    #endregion
+
+    #region MonoBehaviour Methods
+    public void Awake()
+    {
+        ResetCount();
+    }
+    #endregion
+    //Most of the game combos will have different completition conditions. This method will check the combo progress.
+    #region Public methods
+
+    public void EnableCombo()
+    {
+        comboEnabled = true;
+    }
+
+    public void DisableCombo()
+    {
+        comboEnabled = false;
+    }
+
+    public virtual void IncreaseCurrentCount(int addToCount)
+    {
+        currentCount += addToCount;
+    }
+
+    public virtual void ResetCount()
+    {
+        currentCount = 0;
+    }
+
+    public virtual void ReviewConditions()
+    {
+
+    }
+    #endregion
+
+    #region Protected methods
+    protected virtual void GrantReward()
+    {
+        StatsManager.instance.IncreaseGlobalPoints(reward);
+    }
+    #endregion
+}
