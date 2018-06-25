1f6d876461efd71badb65337a73f7c4584e41e2b
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-06-25 23:53:59 +0200

    Randomize the slimes' animation speed
    - Organize all enemy prefabs GameObjects by putting all scripts first (after the Transform).

---
+++ Assets/Scripts/IA/SlimeWalkSpeedRandomizer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class SlimeWalkSpeedRandomizer : MonoBehaviour
+{
+    #region Fields
+    public float minValue = 0.8f;
+    public float maxValue = 1.1f;
+
+    private Animator animator;
+    #endregion
+
+    #region MonoBehaviour Methods
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        UnityEngine.Assertions.Assert.IsNotNull(animator, "ERROR: An Animator Component could not be found by SlimeWalkSpeedRandomizer in GameObject " + gameObject.name);
+    }
+
+    private void Start()
+    {
+        float speed = Random.Range(minValue, maxValue);
+        animator.SetFloat("Speed", speed);
+    }
+    #endregion
+}
