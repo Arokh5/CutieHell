31bbdd0e26547053426a5bd1cbb582553e26464a
Dani Reyes <dani-kingdom@hotmail.com>
2018-06-26 23:14:59 +0200

    Set random animation speed to all animated map props

---
+++ Assets/RandomizeAnimations.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeAnimations : MonoBehaviour {
+
+    [SerializeField]
+    private bool breakIdle;
+    [SerializeField]
+    private bool windExplosion;
+    private Animator animator;
+    private float nextBreakIdle;
+
+    void Awake () {
+        animator = this.GetComponent<Animator>();
+        animator.SetFloat("Speed", Random.Range(0.8f, 1.2f));
+        nextBreakIdle = Random.Range(4.0f, 9.0f);
+	}
+
+	void Update () {
+        if (breakIdle)
+        {
+            nextBreakIdle -= Time.deltaTime;
+            if(nextBreakIdle <= 0.0f)
+            {
+                nextBreakIdle = Random.Range(4.0f, 9.0f);
+                animator.SetTrigger("BreakIdle");
+            }
+        }
+	}
+}
