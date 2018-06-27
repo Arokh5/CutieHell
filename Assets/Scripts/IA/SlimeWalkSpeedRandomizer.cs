1c1240a583367cafabe732b7be8201a397bd012f
Dani Reyes <dani-kingdom@hotmail.com>
2018-06-28 01:23:42 +0200

    Add footsteps to slimes.

--- Assets/Scripts/IA/SlimeWalkSpeedRandomizer.cs
+++ Assets/Scripts/IA/SlimeWalkSpeedRandomizer.cs
@@ -5,6 +5,7 @@ public class SlimeWalkSpeedRandomizer : MonoBehaviour
     #region Fields
     public float minValue = 0.8f;
     public float maxValue = 1.1f;
+    public ParticleSystem footStepsVFX;
 
     private Animator animator;
     #endregion
@@ -22,6 +23,11 @@ public class SlimeWalkSpeedRandomizer : MonoBehaviour
             animator = GetComponent<Animator>();
         float speed = Random.Range(minValue, maxValue);
         animator.SetFloat("Speed", speed);
+    }
+
+    public void InstantiateVFX()
+    {
+        ParticlesManager.instance.LaunchParticleSystem(footStepsVFX, this.transform.position - Vector3.up * 0.05f, footStepsVFX.transform.rotation);
     }
     #endregion
 }
