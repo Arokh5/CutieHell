47ed9b728a4c4db0c044cbd278fea13d38293cfd
Dani Reyes <dani-kingdom@hotmail.com>
2018-07-04 19:21:28 +0200

    New Cone attack range

---
+++ Assets/ConeAttackCameraShake.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZCameraShake;
+
+public class ConeAttackCameraShake : MonoBehaviour {
+
+    private bool shaked;
+    private float timeToShake;
+
+    private void OnEnable()
+    {
+        shaked = false;
+        timeToShake = 0.5f;
+    }
+
+    private void Update()
+    {
+        timeToShake -= Time.deltaTime;
+        if(timeToShake <= 0.0f && !shaked)
+        {
+            shaked = true;
+            CameraShaker.Instance.ShakeOnce(0.3f, 4.5f, 0.1f, 0.7f);
+        }
+    }
+}
