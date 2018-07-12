e2c11b98f993dd58fd81010f2c31682763813550
Dani Reyes <dani-kingdom@hotmail.com>
2018-07-12 17:49:58 +0200

    improve bullet time script so the slow-mo speed is maintained over the desired time

--- Assets/Scripts/General/BulletTime.cs
+++ Assets/Scripts/General/BulletTime.cs
@@ -5,6 +5,8 @@ public class BulletTime : MonoBehaviour {
     public static BulletTime instance;
     public float slowdownFactor = 0.05f;
     public float slowdownLength = 0.5f;
+    public float slowdownIncreaseTime = 0.5f;
+    private float timeOnSlowdown = 0.0f;
 
 	void Awake ()
     {
@@ -17,16 +19,28 @@ public class BulletTime : MonoBehaviour {
 
         if (!GameManager.instance.gameIsPaused && Time.timeScale != 1.0f)
         {
-            Time.timeScale += (1f / slowdownLength) * Time.unscaledDeltaTime;
-            Time.timeScale = Mathf.Clamp(Time.timeScale,0.0f,1.0f);
-            Time.fixedDeltaTime = Time.timeScale * 0.02f;
+            if (timeOnSlowdown < slowdownLength)
+            {
+                timeOnSlowdown += Time.unscaledDeltaTime;
+                Time.timeScale = Mathf.Clamp(Time.timeScale, 0.0f, 1.0f);
+                Time.fixedDeltaTime = Time.timeScale * 0.02f;
+            }
+            else if(timeOnSlowdown < slowdownIncreaseTime + slowdownLength)
+            {
+                timeOnSlowdown += Time.unscaledDeltaTime;
+                Time.timeScale += (1f / slowdownIncreaseTime) * Time.unscaledDeltaTime;
+                Time.timeScale = Mathf.Clamp(Time.timeScale, 0.0f, 1.0f);
+                Time.fixedDeltaTime = Time.timeScale * 0.02f;
+            }
         }
     }
 
-    public void DoSlowmotion(float _slowdownFactor = 0.05f, float _slowdownLength = 0.5f)
+    public void DoSlowmotion(float _slowdownFactor = 0.05f, float _slowdownLength = 0.5f, float _slowdownIncreaseTime = 0.15f)
     {
         Time.timeScale = _slowdownFactor;
         Time.fixedDeltaTime = Time.timeScale * 0.02f;
         slowdownLength = _slowdownLength;
+        slowdownIncreaseTime = _slowdownIncreaseTime;
+        timeOnSlowdown = 0.0f;
     }
 }
