fddaab44b5d01ed317e84dce75f0391abca337dc
Dani Reyes <dani-kingdom@hotmail.com>
2018-07-13 22:10:05 +0200

    Create the base of the meteorite storm

---
+++ Assets/MeteoriteMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteoriteMovement : MonoBehaviour {
+
+    public float speed = 8.0f;
+
+	void Update () {
+        this.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+	}
+}
