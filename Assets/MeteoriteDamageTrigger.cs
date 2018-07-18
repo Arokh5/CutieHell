19d2634f2d1b43a23e7dfce59d90990fbc768a52
Dani Reyes <dani-kingdom@hotmail.com>
2018-07-19 00:27:53 +0200

    Add damage and explosion effect to meteorites

---
+++ Assets/MeteoriteDamageTrigger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteoriteDamageTrigger : MonoBehaviour {
+
+    public float damage;
+    public List<AIEnemy> enemiesInRange;
+    private float delayUntilExplosion;
+
+    private void OnEnable()
+    {
+        delayUntilExplosion = 0.05f;
+        enemiesInRange = new List<AIEnemy>();
+    }
+
+    private void Update()
+    {
+        if(delayUntilExplosion < 0.0f)
+        {
+            HurtEnemies();
+        }
+        else
+        {
+            delayUntilExplosion -= Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == 8)
+        {
+            enemiesInRange.Add(other.GetComponent<AIEnemy>());
+        }
+    }
+
+    private void HurtEnemies()
+    {
+        foreach (AIEnemy aiEnemy in enemiesInRange)
+        {
+            aiEnemy.TakeDamage(damage, AttackType.METEORITE);
+            aiEnemy.SetKnockback(this.transform.position, 1.0f);
+            aiEnemy.SetSlow(0.75f);
+        }
+
+    }
+}
--- Assets/MeteoriteMovement.cs
+++ Assets/MeteoriteMovement.cs
@@ -5,8 +5,24 @@ using UnityEngine;
 public class MeteoriteMovement : MonoBehaviour {
 
     public float speed = 8.0f;
+    public float damage = 4.0f;
+    [SerializeField]
+    private ParticleSystem damageDealer;
+    [SerializeField]
+    private TimedPooledParticleSystem pool;
 
-	void Update () {
+    void Update () {
         this.transform.Translate(Vector3.forward * speed * Time.deltaTime);
 	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 17)
+        {
+            MeteoriteDamageTrigger o = ParticlesManager.instance.LaunchParticleSystem(damageDealer, this.transform.position, damageDealer.transform.rotation).GetComponent<MeteoriteDamageTrigger>();
+            o.damage = 3.0f;
+            pool.ReturnToPool();
+        }
+
+    }
 }
