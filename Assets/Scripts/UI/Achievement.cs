63b95766e8800c4591c046e090c76808e21f4411
Mac092 <m.rodmur@gmail.com>
2018-07-20 08:29:04 +0200

    Add Transition start behaviour and achievement base content

---
+++ Assets/Scripts/UI/Achievement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Achievement : Combo {
+
+    #region Attributes
+    [SerializeField]
+    private TransitionUI achievementTransition;
+    [SerializeField]
+    private Image icon;
+    [SerializeField]
+    private 
+    #endregion
+
+    #region MonoBehaviour methods
+
+    #endregion
+
+    // Use this for initialization
+    void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.H))
+        {
+            GrantReward();
+        }
+	}
+
+    #region Public methods
+    public override void GrantReward()
+    {
+        StatsManager.instance.IncreaseGlobalPoints(reward);
+        TransitionUI.instance.AskForTransition(name, icon);
+    }
+
+    #endregion
+
+    #region Private methods
+
+    #endregion
+}
---
+++ Assets/Scripts/UI/TransitionUI.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public enum TransitionState {TRANSITION_START, TRANSITION_ONGOING, TRANSITION_FINISH}
+public class TransitionUI : MonoBehaviour {
+    public static TransitionUI instance;
+
+    #region Attributes
+    [SerializeField]
+    private GameObject elementToTransition;
+    [SerializeField]
+    private Image background;
+    [SerializeField]
+    private Image[] images;
+    [SerializeField]
+    private string[] texts;
+    [Header("Positions")]
+    [SerializeField]
+    private Transform[] transforms;
+    [Header("Colors")]
+    [SerializeField]
+    private Color[] colors;
+    [SerializeField]
+    private float[] times;
+
+    private bool isTransitionOn = false;
+    private Queue queueImages = new Queue();
+    private Queue queueNames = new Queue();
+
+    private TransitionState transitionState;
+     
+    #endregion
+
+    #region Monobehaviour methods
+
+    #endregion
+
+    // Use this for initialization
+    void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(isTransitionOn)
+        {
+            switch(transitionState)
+            {
+                case (TransitionState.TRANSITION_START):
+                    elementToTransition.transform.position = transforms[0].position;
+                    elementToTransition.transform.localScale = transforms[0].localScale;
+                    background.color = new Color(background.color.r, background.color.g, background.color.b, colors[0].a);
+                    images[0].color = new Color(images[0].color.r, images[0].color.g, images[0].color.b, colors[0].a);
+                    // inicializar posiciones etc
+                    break;
+                case (TransitionState.TRANSITION_ONGOING):
+                    // transición
+                    break;
+                case (TransitionState.TRANSITION_FINISH):
+                    // desinicializar
+                    break;
+            }
+                
+        }
+	}
+	
+	#region Public methods
+	public void AskForTransition(string name, Image image)
+    {
+        if(isTransitionOn)
+        {
+            queueImages.Enqueue(image);
+            queueNames.Enqueue(name);
+        }
+        else
+        {
+            isTransitionOn = true;
+            transitionState = TransitionState.TRANSITION_START;
+            images[0] = image;
+            texts[0] = name;
+        }
+    }
+	#endregion
+	
+	#region Private methods
+	
+	#endregion
+}
