96bc21d9d4cc7142650187fd5fbc570b82eab60e
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-07-31 18:50:13 +0200

    Modify the Palyer Knockback behaviour so it is handled within the DefaultMove StateAction

--- Assets/Scripts/Player State Machine/Actions/State Actions/PlayerMove.cs
+++ Assets/Scripts/Player State Machine/Actions/State Actions/PlayerMove.cs
@@ -16,88 +16,111 @@ public class PlayerMove : StateAction
     {
         if (player.canMove)
         {
-            Vector3 accelerationVector = Vector3.zero;
-            Vector3 verticalAcceleration = player.transform.forward;
-            Vector3 horizontalAcceleration = player.transform.right;
-
-            accelerationVector += verticalAcceleration * -InputManager.instance.GetLeftStickVerticalValue();
-            accelerationVector += horizontalAcceleration * InputManager.instance.GetLeftStickHorizontalValue();
-
-            float accelerationMagnitude = accelerationVector.magnitude;
-            if (accelerationMagnitude > 1.0f)
-            {
-                accelerationVector.Normalize();
-                accelerationMagnitude = 1.0f;
-            }
-
-            if (accelerationMagnitude > 0.1f)
-            {
-                if (useAnimation)
-                    player.animator.SetBool("Move", true);
-                else
-                    player.mainCameraController.timeSinceLastAction = 0.0f;
-
-                if (useFootsteps)
-                {
-                    player.footSteps.SetActive(true);
-                    if (!player.loopAudioSource.isPlaying)
-                        player.loopAudioSource.Play();
-                }
-            }
-            else
-            {
-                accelerationVector = Vector3.zero;
-                accelerationMagnitude = 0.0f;
-                if (useAnimation)
-                    player.animator.SetBool("Move", false);
-
-                if (useFootsteps)
-                {
-                    player.footSteps.SetActive(false);
-                    player.loopAudioSource.Stop();
-                }
-            }
-
             Vector3 playerPos = player.rb.position;
 
-            /* Remove currentSpeed components that are not aligned with acceleration */
-            if (accelerationMagnitude != 0.0f)
-            {
-                float dot = Vector3.Dot(accelerationVector / accelerationMagnitude, player.currentSpeed);
-                if (dot > 0)
-                    player.currentSpeed = (accelerationVector / accelerationMagnitude) * dot;
-                else
-                    player.currentSpeed = Vector3.zero;
-            }
+            if (player.knockbackActive)
+                Knockback(player, ref playerPos);
             else
-                player.currentSpeed = Vector3.zero;
+                Move(player, ref playerPos);
 
-            /* Calculate currentSpeed */
-            player.currentSpeed += acceleration * accelerationVector * Time.deltaTime;
-            if (player.currentSpeed.sqrMagnitude > maxSpeed * maxSpeed)
-                player.currentSpeed = player.currentSpeed.normalized * maxSpeed;
-
-            /* Calculate new position */
-            playerPos += player.currentSpeed * Time.deltaTime;
-
-            /* Adjust ground height */
-            RaycastHit hit;
-            float upOffset = 10.0f;
-            Vector3 forwardOffsetVector = (accelerationMagnitude > 0 ? (0.25f * accelerationVector / accelerationMagnitude) : Vector3.zero);
-            if (Physics.Raycast(player.transform.position + upOffset * Vector3.up + forwardOffsetVector, -Vector3.up, out hit, 50, walkableLayer))
-            {
-                if (Mathf.Abs(hit.distance - upOffset - player.floorClearance) > 0.005f)
-                    playerPos.y -= (hit.distance - upOffset - player.floorClearance);
-
-                player.lastValidPosition = playerPos;
-            }
-            else if (!canExitWalkableLayer)
-            {
-                playerPos = player.lastValidPosition;
-            }
+            CheckGround(player, ref playerPos);
 
             /* Set newly calculated position to rigidbody */
             player.rb.position = playerPos;
+        }
+    }
+
+    private void Move(Player player, ref Vector3 playerPos)
+    {
+        Vector3 accelerationVector = Vector3.zero;
+        Vector3 verticalAcceleration = player.transform.forward;
+        Vector3 horizontalAcceleration = player.transform.right;
+
+        accelerationVector += verticalAcceleration * -InputManager.instance.GetLeftStickVerticalValue();
+        accelerationVector += horizontalAcceleration * InputManager.instance.GetLeftStickHorizontalValue();
+
+        float accelerationMagnitude = accelerationVector.magnitude;
+        if (accelerationMagnitude > 1.0f)
+        {
+            accelerationVector.Normalize();
+            accelerationMagnitude = 1.0f;
+        }
+
+        if (accelerationMagnitude > 0.1f)
+        {
+            if (useAnimation)
+                player.animator.SetBool("Move", true);
+            else
+                player.mainCameraController.timeSinceLastAction = 0.0f;
+
+            if (useFootsteps)
+            {
+                player.footSteps.SetActive(true);
+                if (!player.loopAudioSource.isPlaying)
+                    player.loopAudioSource.Play();
+            }
+        }
+        else
+        {
+            accelerationVector = Vector3.zero;
+            accelerationMagnitude = 0.0f;
+            if (useAnimation)
+                player.animator.SetBool("Move", false);
+
+            if (useFootsteps)
+            {
+                player.footSteps.SetActive(false);
+                player.loopAudioSource.Stop();
+            }
+        }
+
+        /* Remove currentSpeed components that are not aligned with acceleration */
+        if (accelerationMagnitude != 0.0f)
+        {
+            float dot = Vector3.Dot(accelerationVector / accelerationMagnitude, player.currentSpeed);
+            if (dot > 0)
+                player.currentSpeed = (accelerationVector / accelerationMagnitude) * dot;
+            else
+                player.currentSpeed = Vector3.zero;
+        }
+        else
+            player.currentSpeed = Vector3.zero;
+
+        /* Calculate currentSpeed */
+        player.currentSpeed += acceleration * accelerationVector * Time.deltaTime;
+        if (player.currentSpeed.sqrMagnitude > maxSpeed * maxSpeed)
+            player.currentSpeed = player.currentSpeed.normalized * maxSpeed;
+
+        /* Calculate new position */
+        playerPos += player.currentSpeed * Time.deltaTime;
+    }
+
+    private void Knockback(Player player, ref Vector3 playerPos)
+    {
+        player.knockbackCurrentForce = Mathf.Lerp(player.knockbackCurrentForce, 0.0f, 0.5f);
+        playerPos += player.knockbackDirection * player.knockbackForce * Time.deltaTime;
+        if (player.knockbackCurrentForce < 0.2f)
+        {
+            player.knockbackActive = false;
+        }
+    }
+
+    private void CheckGround(Player player, ref Vector3 playerPos)
+    {
+        /* Adjust ground height */
+        RaycastHit hit;
+        float upOffset = 10.0f;
+        //if (Physics.Raycast(player.transform.position + upOffset * Vector3.up + inPlaneOffset, -Vector3.up, out hit, 50, walkableLayer))
+        if (Physics.Raycast(playerPos + upOffset * Vector3.up, -Vector3.up, out hit, 50, walkableLayer))
+        {
+            if (Mathf.Abs(hit.distance - upOffset - player.floorClearance) > 0.005f)
+                playerPos.y -= (hit.distance - upOffset - player.floorClearance);
+
+            player.lastValidPosition = playerPos;
+        }
+        else if (!canExitWalkableLayer)
+        {
+            playerPos = player.lastValidPosition;
         }
     }
 }
