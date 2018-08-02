00a540790967b2cbf11edc37aab67b74f8274060
Dani Reyes <dani-kingdom@hotmail.com>
2018-08-03 00:16:00 +0200

    add base vfx for dash

---
+++ Assets/Scripts/Player State Machine/Actions/Entry Actions/DashEnterAction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Player State Machine/Actions/DashEnterAction")]
+public class DashEnterAction : StateAction
+{
+    [SerializeField]
+    private ParticleSystem dashVFX;
+
+    public override void Act(Player player)
+    {
+        ParticleSystem ps = ParticlesManager.instance.LaunchParticleSystem(dashVFX,player.transform.position,dashVFX.transform.rotation);
+        ps.transform.SetParent(player.transform);
+    }
+}
