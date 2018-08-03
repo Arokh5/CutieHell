2bc18449522f490c8c893f3491ed36c34519ed5d
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-08-03 18:27:30 +0200

    Fix issue where using the dash towards an Environment prop could cause weird behaviour
    - Fix the dash limit calculation (ensure distance are meassure in XZ-plane).
    - Add the Environment layer to the dash limit check.

--- Assets/Scripts/Player State Machine/Decisions/DashEnterDecision.cs
+++ Assets/Scripts/Player State Machine/Decisions/DashEnterDecision.cs
@@ -3,14 +3,13 @@
 [CreateAssetMenu(menuName = "Player State Machine/Decisions/DashEnterDecision")]
 public class DashEnterDecision : Decision
 {
+    public float limitCheckUpwardsOffset = 0.2f;
     public LayerMask dashLimitLayerMask;
 
     public override bool Decide(Player player)
     {
         if (!player.knockbackActive && InputManager.instance.GetL2ButtonDown())
         {
-            Vector3 direction = player.transform.forward;
-
             float threshold = InputManager.joystickThreshold;
             float verticalValue = -InputManager.instance.GetLeftStickVerticalValue();
             if (verticalValue > -threshold && verticalValue < threshold)
@@ -19,23 +18,30 @@ public class DashEnterDecision : Decision
             if (horizontalValue > -threshold && horizontalValue < threshold)
                 horizontalValue = 0.0f;
 
+            Vector3 direction;
             if (horizontalValue != 0.0f || verticalValue != 0.0f)
             {
                 direction = new Vector3(horizontalValue, 0, verticalValue);
                 direction = player.mainCameraController.transform.TransformDirection(direction);
-                direction.Normalize();
             }
+            else
+            {
+                direction = player.transform.forward;
+            }
+
+            direction.y = 0.0f;
+            direction.Normalize();
 
             player.dashDirection = direction;
             player.dashElapsedTime = 0.0f;
 
             // Calculate limit distance
-            float upwardsOffset = 1.0f;
             RaycastHit hit;
-            if (Physics.Raycast(player.transform.position + upwardsOffset * Vector3.up, player.dashDirection, out hit, player.dashDistance, dashLimitLayerMask))
+            if (Physics.Raycast(player.transform.position + limitCheckUpwardsOffset * Vector3.up, player.dashDirection, out hit, player.dashDistance, dashLimitLayerMask))
                 player.dashRemainingDistance = hit.distance;
             else
                 player.dashRemainingDistance = player.dashDistance;
+
             return true;
         }
         return false;
