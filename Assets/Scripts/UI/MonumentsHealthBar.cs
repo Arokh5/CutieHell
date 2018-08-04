9165ee1a412cd20c9c71651f22eafcff8e636108
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-08-04 18:00:44 +0200

    Implement flashing (scale and color) of the MonumentsHealthBar when  health is below a customizable limit

--- Assets/Scripts/UI/MonumentsHealthBar.cs
+++ Assets/Scripts/UI/MonumentsHealthBar.cs
@@ -3,35 +3,79 @@ using UnityEngine.UI;
 
 public class MonumentsHealthBar : MonoBehaviour
 {
+    [System.Serializable]
+    private class FlashInfo
+    {
+        public float flashCycleDuration = 0.5f;
+        [Range(1.0f, 2.0f)]
+        public float scaleFactor = 1.1f;
+        public Color flashTintColor = Color.red;
+    }
+
     #region Fields
     [Header("Element setup")]
+    [SerializeField]
+    private Image backgroundImage;
     [SerializeField]
     private Image healthImage;
     [SerializeField]
     private Text barText;
 
-    [Header("Refill animation setup")]
-    [SerializeField]
-    private float refillAnimationDuration = 1.0f;
-
     private float currentFill = 1.0f;
 
-    private bool animateRefill = true;
+    [Header("Refill animation setup")]
+    [SerializeField]
+    [Tooltip("The time (in seconds) it takes to animate the health bar being refilled.")]
+    private float refillAnimationDuration = 1.0f;
+
+    private bool refilling = true;
     private float refillElapsedTime;
+
+    [Header("Flashing setup")]
+    [SerializeField]
+    [Tooltip("The minimum value for the health bar fill below which the bar begins flashing")]
+    public float flashThreshold = 0.2f;
+    [SerializeField]
+    [Tooltip("The time (in seconds) counted from the last health bar update in which the Strong Flash Info is used. Ohterwise, the Weak Flash Info is used.")]
+    public float strongFlashDuration = 2.0f;
+    [SerializeField]
+    [Tooltip("The flash parameters to use for weak flashing.")]
+    private FlashInfo weakFlashInfo;
+    [SerializeField]
+    [Tooltip("The flash parameters to use for strong flashing.")]
+    private FlashInfo strongFlashInfo;
+
+    private RectTransform rectTransform;
+    private bool flashing = false;
+    private bool shouldStopFlashing = false;
+    private float flashElapsedTime;
+    private FlashInfo currentFlashInfo;
+    private float strongFlashTimeLeft;
+    private Vector3 initialScale;
+    private Color initialColor;
+
     #endregion
 
     #region MonoBehaviour Methods
     private void Awake()
     {
+        UnityEngine.Assertions.Assert.IsNotNull(backgroundImage, "ERROR: Background Image (Image) not assigned for MonumentsHealthBar in gameObject '" + gameObject.name + "'");
         UnityEngine.Assertions.Assert.IsNotNull(healthImage, "ERROR: Health Image (Image) not assigned for MonumentsHealthBar in gameObject '" + gameObject.name + "'");
         UnityEngine.Assertions.Assert.IsNotNull(barText, "ERROR: Bar Text (Text) not assigned for MonumentsHealthBar in gameObject '" + gameObject.name + "'");
+        rectTransform = GetComponent<RectTransform>();
+        UnityEngine.Assertions.Assert.IsNotNull(rectTransform, "ERROR: A RectTransform Component could not be found by MonumentsHealthBar in gameObject '" + gameObject.name + "'");
+
+        initialScale = rectTransform.localScale;
+        initialColor = backgroundImage.color;
     }
 
     private void Update()
     {
-        if (animateRefill)
+        if (refilling)
             RefillAnimation();
 
+        if (flashing)
+            FlashAnimation();
     }
     #endregion
 
@@ -51,17 +95,22 @@ public class MonumentsHealthBar : MonoBehaviour
 
         currentFill = normalizedFill;
 
-        if (!animateRefill)
+        if (!refilling)
             healthImage.fillAmount = normalizedFill;
+
+        if (normalizedFill < flashThreshold && normalizedFill > 0)
+            StartFlashAnimation();
+        else
+            RequestFlashAnimationStop();
     }
 
     public void RefillHealthBar(bool animate = true)
     {
+        RequestFlashAnimationStop();
         currentFill = 1.0f;
         if (animate)
         {
-            animateRefill = true;
-            refillElapsedTime = 0.0f;
+            StartRefillAnimation();
         }
         else
             healthImage.fillAmount = currentFill;
@@ -69,6 +118,12 @@ public class MonumentsHealthBar : MonoBehaviour
     #endregion
 
     #region Private Methods
+    private void StartRefillAnimation()
+    {
+        refilling = true;
+        refillElapsedTime = 0.0f;
+    }
+
     private void RefillAnimation()
     {
         refillElapsedTime += Time.deltaTime;
@@ -78,7 +133,128 @@ public class MonumentsHealthBar : MonoBehaviour
         healthImage.fillAmount = progress * currentFill;
 
         if (refillElapsedTime >= refillAnimationDuration)
-            animateRefill = false;
+            refilling = false;
+    }
+
+    private void StartFlashAnimation()
+    {
+        strongFlashTimeLeft = strongFlashDuration;
+        if (currentFlashInfo == null)
+            currentFlashInfo = strongFlashInfo;
+
+        if (!flashing)
+        {
+            flashing = true;
+            flashElapsedTime = 0.0f;
+        }
+        else
+        {
+            shouldStopFlashing = false;
+        }
+    }
+
+    private void FlashAnimation()
+    {
+        if (strongFlashTimeLeft > 0)
+            strongFlashTimeLeft -= Time.deltaTime;
+
+        flashElapsedTime += Time.deltaTime;
+        float progress = flashElapsedTime / currentFlashInfo.flashCycleDuration;
+
+        bool stopFlashing = false;
+        if (flashElapsedTime >= currentFlashInfo.flashCycleDuration)
+        {
+            // Cycle end
+            flashElapsedTime -= currentFlashInfo.flashCycleDuration;
+            currentFlashInfo = GetCurrentFlashInfo();
+            progress = flashElapsedTime / currentFlashInfo.flashCycleDuration;
+
+            if (shouldStopFlashing)
+            {
+                stopFlashing = true;
+                progress = 0;
+            }
+        }
+
+        UpdateScale(progress);
+        UpdateTint(progress);
+
+        if (stopFlashing)
+            StopFlashAnimation();
+    }
+
+    private void StopFlashAnimation()
+    {
+        if (flashing)
+        {
+            shouldStopFlashing = false;
+            flashing = false;
+            currentFlashInfo = null;
+        }
+    }
+
+    private void RequestFlashAnimationStop()
+    {
+        if (flashing)
+            shouldStopFlashing = true;
+    }
+
+    private FlashInfo GetCurrentFlashInfo()
+    {
+        FlashInfo flashInfo;
+        if (strongFlashTimeLeft > 0)
+            flashInfo = strongFlashInfo;
+        else
+            flashInfo = weakFlashInfo;
+
+        return flashInfo;
+    }
+
+    private void UpdateScale(float cycleProgress)
+    {
+        // The cycle is split in the 1st half (grow), the 2nd half (shrink)
+        float scaleFactor;
+        float localProgress;
+        if (cycleProgress < 0.5f)
+        {
+            // Grow
+            localProgress = cycleProgress / 0.5f;
+            scaleFactor = Interpolate(1.0f, currentFlashInfo.scaleFactor, localProgress);
+        }
+        else
+        {
+            // Shrink
+            localProgress = (cycleProgress - 0.5f) / 0.5f;
+            scaleFactor = Interpolate(currentFlashInfo.scaleFactor, 1.0f, localProgress);
+        }
+
+        rectTransform.localScale = initialScale * scaleFactor;
+    }
+
+    private void UpdateTint(float cycleProgress)
+    {
+        // The cycle is split in 1st half (tint gain), the 2nd half (tint loss)
+        float tintFactor;
+        float localProgress;
+        if (cycleProgress < 0.5f)
+        {
+            // Tint gain
+            localProgress = cycleProgress / 0.5f;
+            tintFactor = Interpolate(0.0f, 1.0f, localProgress);
+        }
+        else
+        {
+            // Tint loss
+            localProgress = (cycleProgress - 0.5f) / 0.5f;
+            tintFactor = Interpolate(1.0f, 0.0f, localProgress);
+        }
+
+        backgroundImage.color = initialColor + (tintFactor * currentFlashInfo.flashTintColor);
+    }
+
+    private float Interpolate(float start, float end, float u)
+    {
+        return start * (1 - u) + end * u;
     }
     #endregion
 }
