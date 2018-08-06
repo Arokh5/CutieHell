4776bc70ec4d9be728d8300496e44a41c71ee49a
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-08-06 19:21:00 +0200

    Implement the AttackInfosManager and the AttackInfos to join information about an attack and its related ControllerButton and button sprite

---
+++ Assets/Scripts/AttackChains/AttackInfosManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class AttackInfosManager : MonoBehaviour
+{
+    #region Fields
+    public static AttackInfosManager instance;
+
+    public AttackInfo[] attackInfos;
+    #endregion
+
+    #region MonoBehaviour Methods
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(this);
+    }
+    #endregion
+}
