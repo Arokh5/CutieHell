4c4cfd9c9a98229935c8baf18b9e0fc102fccd7e
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-08-06 18:06:54 +0200

    Implement the basic functionality of the AttackChainsManager and the AttackChain

--- Assets/Scripts/AttackChains/AttackChain.cs
+++ Assets/Scripts/AttackChains/AttackChain.cs
@@ -3,10 +3,87 @@
 [System.Serializable]
 public class AttackChain
 {
+    #region Fields
     [Header("Attack Chain")]
     public string name;
     public AttackType startAttack;
     public FollowUpAttack[] followUps;
-    [HideInInspector]
-    public FollowUpAttack currentFollowUp = null;
+
+    private int followUpIndex = -1;
+    #endregion
+
+    #region Public Methods
+    public bool StartChain(AttackType attack)
+    {
+        bool success = false;
+        if (followUps.Length != 0 && startAttack == attack)
+        {
+            followUpIndex = 0;
+            success = true;
+        }
+        return success;
+    }
+
+    /// <summary>
+    /// Returns true if the attack is the next expected attack in the chain. This method does NOT alter the state of the chain.
+    /// </summary>
+    /// <param name="attack">The AttackType to test for chaining</param>
+    public bool CanChain(AttackType attack)
+    {
+        FollowUpAttack currentFollowUp = GetCurrentFollowUpAttack();
+        if (currentFollowUp != null)
+        {
+            return currentFollowUp.attack == attack;
+        }
+        return false;
+    }
+    
+    /// <summary>
+    /// Returns true if the Chain is successfully advanced.
+    /// Returns false if the chain had not been started, or if the chain has reached its end.
+    /// In the latter, the chain is put back into an unstarted state.
+    /// </summary>
+    public bool AdvanceChain()
+    {
+        bool success = false;
+
+        if (followUpIndex != -1)
+        {
+            if (followUpIndex + 1 < followUps.Length)
+            {
+                ++followUpIndex;
+                success = true;
+            }
+            else
+            {
+                ResetChain();
+            }
+        }
+
+        return success;
+    }
+
+    public void ResetChain()
+    {
+        followUpIndex = -1;
+    }
+
+    public State GetFollowUpState()
+    {
+        if (followUpIndex != -1)
+            return followUps[followUpIndex].relatedState;
+        else
+            return null;
+    }
+    #endregion
+
+    #region Private Methods
+    private FollowUpAttack GetCurrentFollowUpAttack()
+    {
+        if (followUpIndex != -1)
+            return followUps[followUpIndex];
+        else
+            return null;
+    }
+    #endregion
 }
--- Assets/Scripts/AttackChains/AttackChainsManager.cs
+++ Assets/Scripts/AttackChains/AttackChainsManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class AttackChainsManager : MonoBehaviour
 {
@@ -7,6 +8,8 @@ public class AttackChainsManager : MonoBehaviour
 
     [SerializeField]
     private AttackChain[] attackChains;
+
+    private List<AttackChain> activeChains = new List<AttackChain>();
     #endregion
 
     #region MonoBehaviour Methods
@@ -22,17 +25,68 @@ public class AttackChainsManager : MonoBehaviour
     #region Public Methods
     public void ReportAttack(AttackType attack)
     {
-        throw new System.NotImplementedException("ERROR: AttackChainsManager::ReportAttack NOT implemented!");
+        if (HasActiveChains())
+        {
+            List<AttackChain> chainsToRemove = new List<AttackChain>();
+            foreach (AttackChain chain in activeChains)
+            {
+                if (chain.CanChain(attack))
+                {
+                    if (!chain.AdvanceChain())
+                    {
+                        chainsToRemove.Add(chain);
+                    }
+                }
+                else
+                {
+                    chainsToRemove.Add(chain);
+                }
+            }
+
+            foreach (AttackChain chain in chainsToRemove)
+            {
+                activeChains.Remove(chain);
+            }
+        }
+        else
+        {
+            foreach (AttackChain chain in attackChains)
+            {
+                if (chain.StartChain(attack))
+                    activeChains.Add(chain);
+            }
+        }
     }
 
     public bool TestChain(AttackType attack)
     {
-        throw new System.NotImplementedException("ERROR: AttackChainsManager::TestChain NOT implemented!");
+        foreach (AttackChain chain in activeChains)
+        {
+            if (chain.CanChain(attack))
+                return true;
+        }
+        return false;
     }
 
+    /// <summary>
+    /// Returns the State for the first AttackChain that is active
+    /// </summary>
+    /// <returns></returns>
     public State GetNextChainState()
     {
-        throw new System.NotImplementedException("ERROR: AttackChainsManager::GetNextChainState NOT implemented!");
+        if (HasActiveChains())
+        {
+            return activeChains[0].GetFollowUpState();
+        }
+        return null;
+    }
+    #endregion
+
+    #region Private Methods
+
+    private bool HasActiveChains()
+    {
+        return activeChains.Count != 0;
     }
     #endregion
 }
