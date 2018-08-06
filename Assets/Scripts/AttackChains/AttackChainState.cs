43da97c48084a8692fb143f7e8745a33befd53e4
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-08-06 16:49:14 +0200

    Set up the AttackChainsManager interface and implement the AttackChainState

---
+++ Assets/Scripts/AttackChains/AttackChainState.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class AttackChainState : State
+{
+    #region Fields
+    private State chainState;
+    #endregion
+
+    #region Public Methods
+    public override void EnterState(Player player)
+    {
+        chainState = AttackChainsManager.instance.GetNextChainState();
+        chainState.EnterState(player);
+    }
+
+    public override void UpdateState(Player player)
+    {
+        chainState.UpdateState(player);
+    }
+
+    public override void ExitState(Player player)
+    {
+        chainState.ExitState(player);
+        chainState = null;
+    }
+    #endregion
+}
---
+++ Assets/Scripts/AttackChains/AttackChainsManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class AttackChainsManager : MonoBehaviour
+{
+    #region Fields
+    public static AttackChainsManager instance;
+    #endregion
+
+    #region MonoBehaviour Methods
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(this);
+    }
+    #endregion
+
+    #region Public Methods
+    public void ReportAttack(AttackType attack)
+    {
+        throw new System.NotImplementedException("ERROR: AttackChainsManager::ReportAttack NOT implemented!");
+    }
+
+    public bool TestChain(AttackType attack)
+    {
+        throw new System.NotImplementedException("ERROR: AttackChainsManager::TestChain NOT implemented!");
+    }
+
+    public State GetNextChainState()
+    {
+        throw new System.NotImplementedException("ERROR: AttackChainsManager::GetNextChainState NOT implemented!");
+    }
+    #endregion
+}
