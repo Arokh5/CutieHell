ba8c72381707b6cff25d531efe8ab52323492b88
Mac092 <m.rodmur@gmail.com>
2018-08-19 16:29:43 +0200

    Create buttonPadController script to allow UI changes for single buttons based on the current controller, and add this feature to the health's smash button and also to the continue button on round score popup

---
+++ Assets/Scripts/UI/ButtonPadController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonPadController : Observer {
+
+    #region Attributes
+    [SerializeField]
+    private Sprite xboxButtonSprite;
+    [SerializeField]
+    private Sprite ps4ButtonSprite;
+    [SerializeField]
+    private Image buttonPadImage;
+	#endregion
+	
+	#region MonoBehaviour methods
+	
+	#endregion
+	
+	// Use this for initialization
+	void Start () 
+	{
+        InputManager.instance.AddObserver(this);
+
+        if (InputManager.instance.isPS4)
+        {
+            ChangeButtonSprite(ps4ButtonSprite);
+        }
+        else
+        {
+            ChangeButtonSprite(xboxButtonSprite);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+    #region Public methods
+    public override void OnNotify()
+    {
+        if(InputManager.instance.isPS4)
+        {
+            ChangeButtonSprite(ps4ButtonSprite);
+        }
+        else
+        {
+            ChangeButtonSprite(xboxButtonSprite);
+        }
+        
+    }
+    #endregion
+
+    #region Private methods
+    private void ChangeButtonSprite(Sprite sprite)
+    {
+        buttonPadImage.sprite = sprite;
+    }
+    #endregion
+}
