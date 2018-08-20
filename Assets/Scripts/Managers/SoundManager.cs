5ac23411cc31c0e68e8b00bcd51e41135e4734b4
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-08-20 20:02:07 +0200

    Modify the SoundManager script to limit AudioClip reproduction to a customizable amount of times within a customizable time per AudioClip

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -3,8 +3,48 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    private class UsageInfo
+    {
+        private List<float> timers = new List<float>();
+
+        public void UpdateTimers(float deltaTime)
+        {
+            for (int i = timers.Count - 1; i >= 0 ; --i)
+            {
+                float timer = timers[i];
+                timer -= deltaTime;
+                timers[i] = timer;
+                if (timer <= 0)
+                    timers.RemoveAt(i);
+            }
+        }
+
+        public void AddTimer(float timerStart)
+        {
+            if (timerStart > 0)
+                timers.Add(timerStart);
+            else
+                Debug.LogWarning("WARNING: SoundManager::UsageInfo::AddTimer called with a negative value for timerStart. No timer will be started!");
+        }
+
+        public int GetCount()
+        {
+            return timers.Count;
+        }
+    }
+
     #region Fields
     public static SoundManager instance;
+
+    [Header("SFX Repetition limits")]
+    [SerializeField]
+    [Tooltip("The time (in seconds) after playback during which a clip is considered to be 'active'")]
+    private float activeSoundTime = 0.2f;
+    [SerializeField]
+    [Tooltip("The maximum number of 'active' instance of a clip that are allowed to play")]
+    private int maxRepeats = 2;
+    
+    private Dictionary<AudioClip, UsageInfo> activeClips = new Dictionary<AudioClip, UsageInfo>();
 
     [Header("Sources setup")]
     [SerializeField]
@@ -13,7 +53,6 @@ public class SoundManager : MonoBehaviour
     private AudioSource ambienceSource;
     [SerializeField]
     private AudioSource sfxSource;
-
 	#endregion
 	
 	#region MonoBehaviour Methods
@@ -24,10 +63,26 @@ public class SoundManager : MonoBehaviour
         else if (instance != this)
             Destroy(this);
     }
-	#endregion
-	
-	#region Public Methods
 
+    private void Update()
+    {
+        foreach (UsageInfo usageInfo in activeClips.Values)
+        {
+            usageInfo.UpdateTimers(Time.deltaTime);
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (activeSoundTime < 0.0f)
+            activeSoundTime = 0.0f;
+
+        if (maxRepeats < 1)
+            maxRepeats = 1;
+    }
+    #endregion
+
+    #region Public Methods
     public void PlayMusicClip(AudioClip musicClip, float pitch = 1f)
     {
         musicSource.clip = musicClip;
@@ -69,7 +124,28 @@ public class SoundManager : MonoBehaviour
     #region Private Methods
     private bool CanPlay(AudioClip clip)
     {
-        return true;
+        bool canPlay = true;
+
+        UsageInfo usageInfo;
+        if (activeClips.TryGetValue(clip, out usageInfo))
+        {
+            if (usageInfo.GetCount() < maxRepeats)
+            {
+                usageInfo.AddTimer(activeSoundTime);
+            }
+            else
+            {
+                canPlay = false;
+            }
+        }
+        else
+        {
+            usageInfo = new UsageInfo();
+            usageInfo.AddTimer(activeSoundTime);
+            activeClips.Add(clip, usageInfo);
+        }
+
+        return canPlay;
     }
     #endregion
 }
