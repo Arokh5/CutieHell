761532bf0d174986d566e9642756001510861f93
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-08-21 19:31:17 +0200

    Implement the Meteorite attack as an instant StateAction (meteorites drop on the player position)

--- Assets/Scripts/Player State Machine/Actions/State Actions/MeteoriteAim.cs
+++ Assets/Scripts/Player State Machine/Actions/State Actions/MeteoriteAim.cs
@@ -6,52 +6,10 @@ using EZCameraShake;
 [CreateAssetMenu(menuName = "Player State Machine/Actions/MeteoriteAim")]
 public class MeteoriteAim : StateAction
 {
-    public int damage;
     public ParticleSystem meteoritePrefab;
-    public GameObject meteoriteDestinationPrefab;
-    private GameObject meteoriteDestination;
-    public float maxSpeed, acceleration;
 
     public override void Act(Player player)
     {
-        //Vector3 accelerationVector = Vector3.zero;
-        //Vector3 verticalAcceleration = player.transform.forward;
-        //Vector3 horizontalAcceleration = player.transform.right;
-
-        //accelerationVector += verticalAcceleration * -InputManager.instance.GetLeftStickVerticalValue();
-        //accelerationVector += horizontalAcceleration * InputManager.instance.GetLeftStickHorizontalValue();
-
-        //float accelerationMagnitude = accelerationVector.magnitude;
-        //if (accelerationMagnitude > 0.1f)
-        //{
-        //    if (accelerationMagnitude > 1.0f)
-        //    {
-        //        accelerationVector.Normalize();
-        //        accelerationMagnitude = 1.0f;
-        //    }
-        //    Vector3 playerPos = player.rb.position;
-        //    player.mainCameraController.timeSinceLastAction = 0.0f;
-        //    if (accelerationMagnitude != 0.0f)
-        //    {
-        //        float dot = Vector3.Dot(accelerationVector / accelerationMagnitude, player.currentSpeed);
-        //        if (dot > 0)
-        //            player.currentSpeed = (accelerationVector / accelerationMagnitude) * dot;
-        //        else
-        //            player.currentSpeed = Vector3.zero;
-        //    }
-        //    else
-        //        player.currentSpeed = Vector3.zero;
-
-        //    /* Calculate currentSpeed */
-        //    player.currentSpeed += acceleration * accelerationVector * Time.deltaTime;
-        //    if (player.currentSpeed.sqrMagnitude > maxSpeed * maxSpeed)
-        //        player.currentSpeed = player.currentSpeed.normalized * maxSpeed;
-
-        //    /* Calculate new position */
-        //    playerPos += player.currentSpeed * Time.deltaTime;
-
-        //    player.rb.position = playerPos;
-        //}
         RaycastHit hit;
         int layerMask = 1 << 17;
         if (Physics.Raycast(player.mainCamera.transform.position, player.mainCamera.transform.forward, out hit, Mathf.Infinity, layerMask))
@@ -78,6 +36,5 @@ public class MeteoriteAim : StateAction
             player.transform.position = player.meteoritesReturnPlayerPosition[player.currentZonePlaying].position;
             player.mainCameraController.y = 0.0f;
         }
-        
     }
 }
---
+++ Assets/Scripts/Player State Machine/Actions/State Actions/PlaceMeteorite.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Player State Machine/Actions/PlaceMeteorite")]
+public class PlaceMeteorite : StateAction
+{
+    public ParticleSystem meteoritePrefab;
+
+    public override void Act(Player player)
+    {
+        if (InputManager.instance.GetTriangleButtonDown())
+        {
+            if (player.meteoriteAttackCooldown.timeSinceLastAction >= player.meteoriteAttackCooldown.cooldownTime)
+            {
+                player.meteoriteAttackCooldown.timeSinceLastAction = 0.0f;
+                ParticlesManager.instance.LaunchParticleSystem(meteoritePrefab, player.transform.position, meteoritePrefab.transform.rotation);
+            }
+            else
+                player.meteoriteAttackCooldown.cooldownUI.Flash();
+        }
+    }
+}
