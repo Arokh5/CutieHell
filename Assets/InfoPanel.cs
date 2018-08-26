c4ec40a42b93e541aeb92e122b7b73a52804c57e
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-08-26 16:33:53 +0200

    Create a test scene, a testing panel and the base of its managing script

---
+++ Assets/InfoPanel.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoPanel : MonoBehaviour
+{
+    #region Fields
+    [Range(0.0f, 1.0f)]
+    public float alpha;
+    private Graphic[] graphics;
+    #endregion
+
+    #region MonoBehaviour Methods
+    private void Awake()
+    {
+        graphics = GetComponentsInChildren<Graphic>();
+    }
+
+    private void OnValidate()
+    {
+        if (graphics == null)
+            graphics = GetComponentsInChildren<Graphic>();
+
+        SetAlpha(alpha);
+    }
+    #endregion
+
+    #region Private Methods
+    private void SetAlpha(float alpha)
+    {
+        foreach (Graphic graphic in graphics)
+        {
+            Color c = graphic.color;
+            c.a = alpha;
+            graphic.color = c;
+        }
+    }
+    #endregion
+}
