5a6faf26b29d58ca74cac1fe5f92d400ca206c95
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-08-28 18:05:10 +0200

    Implement a delay between hits of the slash attack

--- Assets/Scripts/combat-system/ConeAttackBehaviour.cs
+++ Assets/Scripts/combat-system/ConeAttackBehaviour.cs
@@ -8,10 +8,19 @@ public class ConeAttackBehaviour : PooledParticleSystem
     public ConeAttackDetection enemiesDetector;
     public int enemiesToCombo;
     public float hurtEnemiesDelay;
-    public float timeToDistable;
+    public float timeToDisable;
+
+    public bool hitOverTime;
+    [Tooltip("The time (in seconds) over which the enemies are hit")]
+    public float hitSpreadDuration;
+
     private int comboCount;
     private float timer;
     private float timeToReturnToPool;
+
+    private float hitWaitTime;
+    private float timeToNextHit;
+    private bool hittingOverTime;
 
     #endregion
 
@@ -22,12 +31,39 @@ public class ConeAttackBehaviour : PooledParticleSystem
         timeToReturnToPool -= Time.deltaTime;
         if (timer <= 0.0f)
         {
-            HurtEnemies();
+            LaunchBulletTime();
+            if (hitOverTime)
+            {
+                SetUpHitOverTime();
+            }
+            else
+            {
+                HitAll();
+            }
             timer = 1000.0f;
         }
+
+        if (hittingOverTime)
+        {
+            HitOverTime();
+        }
+
         if(timeToReturnToPool <= 0.0f)
         {
+            HitAll();
             ReturnToPool();
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (hitSpreadDuration < 0.0f)
+            hitSpreadDuration = 0.0f;
+
+        if (hitSpreadDuration > timeToDisable - hurtEnemiesDelay)
+        {
+            Debug.LogWarning("WARNING: (ConeAttackBehaviour): Can't set the HitSpreadDuration to a value larger than the 'Time To Disable' minus the 'Hurt Enemies Delay'!");
+            hitSpreadDuration = timeToDisable - hurtEnemiesDelay;
         }
     }
     #endregion
@@ -37,26 +73,62 @@ public class ConeAttackBehaviour : PooledParticleSystem
     {
         enemiesDetector.attackTargets.Clear();
         timer = hurtEnemiesDelay;
-        timeToReturnToPool = timeToDistable;
+        timeToReturnToPool = timeToDisable;
         comboCount = 0;
+
+        hitWaitTime = 0.0f;
+        hittingOverTime = false;
     }
     #endregion
 
     #region Private Methods
-    private void HurtEnemies()
+    private void LaunchBulletTime()
     {
         if(enemiesDetector.attackTargets.Count > 0)
         {
             BulletTime.instance.DoSlowmotion(0.01f,0.1f,0.05f);
         }
+    }
+
+    private void SetUpHitOverTime()
+    {
+        if (enemiesDetector.attackTargets.Count > 0)
+        {
+            hitWaitTime = hitSpreadDuration / enemiesDetector.attackTargets.Count;
+            timeToNextHit = 0.0f;
+            hittingOverTime = true;
+        }
+    }
+
+    private void HitOverTime()
+    {
+        timeToNextHit -= Time.deltaTime;
+        if (timeToNextHit <= 0.0f)
+        {
+            timeToNextHit += hitWaitTime;
+            AIEnemy aiEnemy = enemiesDetector.attackTargets[0];
+            enemiesDetector.attackTargets.RemoveAt(0);
+            HitOne(aiEnemy);
+        }
+        if (enemiesDetector.attackTargets.Count == 0)
+            hittingOverTime = false;
+    }
+
+    private void HitAll()
+    {
         foreach (AIEnemy aiEnemy in enemiesDetector.attackTargets)
         {
-            aiEnemy.MarkAsTarget(false);
-            aiEnemy.SetKnockback(this.transform.position,6.0f);
-            aiEnemy.TakeDamage(damage, AttackType.CONE);
-            comboCount++;
+            HitOne(aiEnemy);
         }
         enemiesDetector.attackTargets.Clear();
+    }
+
+    private void HitOne(AIEnemy aiEnemy)
+    {
+        aiEnemy.MarkAsTarget(false);
+        aiEnemy.SetKnockback(transform.position, 6.0f);
+        aiEnemy.TakeDamage(damage, AttackType.CONE);
+        comboCount++;
     }
     #endregion
 }
