336536b721c8ca6437ff6f16c587f1d6d75734b3
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-08-29 19:22:38 +0200

    Implement the GameObjectActivation and LaunchParticles ScriptedAnimation elements

---
+++ Assets/Scripts/ZoneTransition/GameObjectActivation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class GameObjectActivation : ScriptedAnimation
+{
+    #region
+    [SerializeField]
+    [Tooltip("The array of GameObjects to be activated or deactivated depending on the ActiveState field")]
+    private GameObject[] gameObjects;
+    [SerializeField]
+    [Tooltip("Indicates whether the GameObjects should be activated or deactivated")]
+    private bool activeState;
+    #endregion
+
+    #region Protected Methods
+    protected override void StartAnimationInternal()
+    {
+        if (gameObjects != null)
+        {
+            foreach (GameObject go in gameObjects)
+                go.SetActive(activeState);
+        }
+
+        OnAnimationFinished();
+    }
+    #endregion
+}
---
+++ Assets/Scripts/ZoneTransition/LaunchParticles.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class LaunchParticles : ScriptedAnimation
+{
+    #region
+    [SerializeField]
+    [Tooltip("The array of ParticleSystems to be launched")]
+    private new ParticleSystem particleSystem;
+    [SerializeField]
+    [Tooltip("Reference used to determine the position where the Particle System will be launched. If none is provided, the Fallback Position is used.")]
+    private Transform referenceTransform;
+    [SerializeField]
+    [Tooltip("This position is only used if no Reference Transform is provided")]
+    private Vector3 fallbackPosition;
+    #endregion
+
+    #region Protected Methods
+    protected override void StartAnimationInternal()
+    {
+        if (particleSystem != null)
+        {
+            Vector3 position = referenceTransform ? referenceTransform.position : fallbackPosition;
+            ParticlesManager.instance.LaunchParticleSystem(particleSystem, position, particleSystem.transform.rotation);
+        }
+        
+        OnAnimationFinished();
+    }
+    #endregion
+}
