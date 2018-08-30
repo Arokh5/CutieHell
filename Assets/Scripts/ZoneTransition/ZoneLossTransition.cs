3e1842b4b3621c786b1cb4805793d492ae645931
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-08-30 17:39:49 +0200

    Add Cinematic Stripes and UI hiding to the ZoneLossTransition logic

--- Assets/Scripts/ZoneTransition/ZoneLossTransition.cs
+++ Assets/Scripts/ZoneTransition/ZoneLossTransition.cs
@@ -3,7 +3,14 @@
 public class ZoneLossTransition : MonoBehaviour
 {
     public delegate void VoidCallback();
+
     #region Fields
+    [SerializeField]
+    private CinematicStripes cinematicStripes;
+    [SerializeField]
+    [Tooltip("GameObjects that will be deactivated for the duration of the Transition.")]
+    private GameObject[] objectsToHide;
+
     [SerializeField]
     private ScriptedAnimation[] scriptedAnimations;
 
@@ -13,15 +20,18 @@ public class ZoneLossTransition : MonoBehaviour
     #endregion
 
     #region MonoBehaviour Methods
-
+    private void Awake()
+    {
+        UnityEngine.Assertions.Assert.IsNotNull(cinematicStripes, "ERROR: Cinematic Stripes (CinematicStripes) not assigned for ZoneLossTransition script in GameObject " + gameObject.name);
+    }
     #endregion
 
     #region Public Methods
     public void StartTransition(VoidCallback voidCallback)
     {
         endCallback = voidCallback;
-        StartAnimationChain();
         inTransition = true;
+        StartAnimationChain();
     }
     #endregion
 
@@ -30,8 +40,10 @@ public class ZoneLossTransition : MonoBehaviour
     {
         Debug.Log("Starting chain");
         currentAnimationIndex = -1;
-        if (scriptedAnimations != null && scriptedAnimations.Length > 0)
+        if (HasAnimations())
         {
+            cinematicStripes.ShowAnimated();
+            SetObjectsToHideActiveState(false);
             StartNextAnimation();
         }
         else
@@ -58,10 +70,31 @@ public class ZoneLossTransition : MonoBehaviour
     private void OnTransitionFinished()
     {
         Debug.Log("Finishing chain");
+        if (HasAnimations())
+        {
+            cinematicStripes.HideAnimated();
+            SetObjectsToHideActiveState(true);
+        }
         inTransition = false;
         VoidCallback callback = endCallback;
         endCallback = null;
         callback();
+    }
+
+    private bool HasAnimations()
+    {
+        return scriptedAnimations != null && scriptedAnimations.Length > 0;
+    }
+
+    private void SetObjectsToHideActiveState(bool activeState)
+    {
+        if (objectsToHide != null)
+        {
+            foreach (GameObject go in objectsToHide)
+            {
+                go.SetActive(activeState);
+            }
+        }
     }
     #endregion
 }
