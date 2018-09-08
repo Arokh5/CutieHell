6908780028d8cdde5b9971e578414008909be122
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-09-08 18:59:55 +0200

    Integrate all RoundInfo elements as part of the minimap

--- Assets/Scripts/UI/FillIndicator.cs
+++ Assets/Scripts/UI/FillIndicator.cs
@@ -4,24 +4,22 @@ using UnityEngine.UI;
 public class FillIndicator : MonoBehaviour
 {
     #region Fields
-    private RawImage rawImage;
+    private Image image;
     private Material material;
     #endregion
 
     #region MonoBehaviour Methods
     private void Awake()
     {
-        rawImage = GetComponent<RawImage>();
-        UnityEngine.Assertions.Assert.IsNotNull(rawImage, "ERROR: A RawImage Component could not be found by FillIndicator in GameObject " + gameObject.name);
-        material = new Material(rawImage.material);
-        rawImage.material = material;
+        image = GetComponent<Image>();
+        UnityEngine.Assertions.Assert.IsNotNull(image, "ERROR: An Image Component could not be found by FillIndicator in GameObject " + gameObject.name);
     }
     #endregion
 
     #region Public Methods
     public void SetFill(float normalizedFill)
     {
-        material.SetFloat("_Fill", normalizedFill);
+        image.fillAmount = normalizedFill;
     }
     #endregion
 }
