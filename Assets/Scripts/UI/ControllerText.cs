eb49f0043d64b2b811b890952aad81914b3b30a5
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-09-17 22:04:20 +0200

    Create the ControllerText script to be able to customize UI texts depending on the type of controller (PS4 or XBox)

---
+++ Assets/Scripts/UI/ControllerText.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ControllerText : Observer
+{
+    #region Fields
+    [SerializeField]
+    private Text textElement;
+    [SerializeField]
+    private string textPS4;
+    [SerializeField]
+    private string textXbox;
+    #endregion
+
+    #region MonoBehaviour Methods
+    private void Awake()
+    {
+        UnityEngine.Assertions.Assert.IsNotNull(textElement, "ERROR: Text Element (Text) not assigned for ControllerSprite script in GameObject " + gameObject.name);
+        InputManager.instance.AddObserver(this);
+        OnNotify();
+    }
+
+    private void OnValidate()
+    {
+        if (textElement)
+        {
+            textElement.text = textPS4;
+        }
+    }
+    #endregion
+
+    #region Public Methods
+    // Observer
+    public override void OnNotify()
+    {
+        if (InputManager.instance.isPS4)
+            textElement.text = textPS4;
+        else if (InputManager.instance.isXbox)
+            textElement.text = textXbox;
+    }
+    #endregion
+}
