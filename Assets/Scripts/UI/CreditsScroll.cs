0d808db9f59188ad38f4402077f781ea4d7266a3
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-09-18 16:44:10 +0200

    Modify the CreditsScroll speed to limit the scrolling, to loop the credits and to be able to customize the scroll speed from the Inspector

--- Assets/Scripts/UI/CreditsScroll.cs
+++ Assets/Scripts/UI/CreditsScroll.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class CreditsScroll : MonoBehaviour {
-
+public class CreditsScroll : MonoBehaviour
+{
+    [Header("Elements setup")]
     [SerializeField]
     private RectTransform credits;
     [SerializeField]
@@ -11,6 +12,14 @@ public class CreditsScroll : MonoBehaviour {
     private AudioSource audioSource;
     [SerializeField]
     private AudioClip backClip;
+
+    [Header("Configuration")]
+    [SerializeField]
+    private float scrollLimit = 3000.0f;
+    [SerializeField]
+    private int normalScrollSpeed = 50;
+    [SerializeField]
+    private int forcedScrollSpeed = 200;
 
 	void Start ()
     {
@@ -28,25 +37,32 @@ public class CreditsScroll : MonoBehaviour {
         Vector3 nextPos = credits.localPosition;
         if (InputManager.instance.GetPadUp() || InputManager.instance.GetLeftStickUp() || InputManager.instance.GetRightStickUp())
         {
-            nextPos.y += 200 * Time.deltaTime;
+            nextPos.y += forcedScrollSpeed * Time.deltaTime;
         }
         else if (InputManager.instance.GetPadDown() || InputManager.instance.GetLeftStickDown() || InputManager.instance.GetRightStickDown())
         {
-            nextPos.y -= 200 * Time.deltaTime;
+            nextPos.y -= forcedScrollSpeed * Time.deltaTime;
         }
         else
         {
-            nextPos.y += 50 * Time.deltaTime;
+            nextPos.y += normalScrollSpeed * Time.deltaTime;
+        }
+
+        if (nextPos.y < 0.0f)
+        {
+            nextPos.y = 0.0f;
+        }
+
+        if (nextPos.y > scrollLimit)
+        {
+            nextPos.y = 0.0f;
         }
         credits.localPosition = nextPos;
     }
 
     private void GoBack()
     {
-        if (InputManager.instance.GetButtonDown(ControllerButton.X) ||
-            InputManager.instance.GetButtonDown(ControllerButton.CIRCLE) ||
-            InputManager.instance.GetButtonDown(ControllerButton.SQUARE) ||
-            InputManager.instance.GetButtonDown(ControllerButton.TRIANGLE))
+        if (InputManager.instance.GetButtonDown(ControllerButton.CIRCLE))
         {
             audioSource.PlayOneShot(backClip);
             fader.FadeToOpaque(0.5f, LoadTitleScreen);
